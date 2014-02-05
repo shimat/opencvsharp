51dabc3f3ff3c2bfd08338eaf370cf65bd7ef7a2
shimat <shima@33i.co.jp>
2014-02-05 19:53:20 +0900

    Added InputArray / OutputArray (don't work)

---
+++ src/OpenCvSharpExtern/imgproc.h
@@ -0,0 +1,47 @@
+/*
+ * (C) 2008-2014 shimat
+ * This code is licenced under the LGPL.
+ */
+
+#ifndef _CPP_IMGPROC_H_
+#define _CPP_IMGPROC_H_
+
+#include "include_opencv.h"
+
+
+CVAPI(void) imgproc_cvtColor(cv::_InputArray *src, cv::_OutputArray *dst, int code, int dstCn)
+{
+	cv::cvtColor(*src, *dst, code, dstCn);
+}
+
+CVAPI(void) imgproc_copyMakeBorder(cv::_InputArray *src, cv::_OutputArray *dst, int top, int bottom, int left, int right, int borderType, CvScalar value)
+{
+	cv::copyMakeBorder(*src, *dst, top, bottom, left, right, borderType, value);
+}
+
+CVAPI(void) imgproc_medianBlur(cv::_InputArray *src, cv::_OutputArray *dst, int ksize)
+{
+	cv::medianBlur(*src, *dst, ksize);
+}
+
+CVAPI(void) imgproc_GaussianBlur(cv::_InputArray *src, cv::_OutputArray *dst, CvSize ksize, double sigmaX, double sigmaY, int borderType)
+{
+	cv::GaussianBlur(*src, *dst, ksize, sigmaX, sigmaY, borderType);
+}
+
+CVAPI(void) imgproc_bilateralFilter(cv::_InputArray *src, cv::_OutputArray *dst, int d, double sigmaColor, double sigmaSpace, int borderType)
+{
+	cv::bilateralFilter(*src, *dst, d, sigmaColor, sigmaSpace, borderType);
+}
+
+CVAPI(void) imgproc_boxFilter(cv::_InputArray *src, cv::_OutputArray *dst, int ddepth, CvSize ksize, CvPoint anchor, int normalize, int borderType)
+{
+	cv::boxFilter(*src, *dst, ddepth, ksize, anchor, normalize != 0, borderType);
+}
+
+CVAPI(void) imgproc_blur(cv::_InputArray *src, cv::_OutputArray *dst, CvSize ksize, CvPoint anchor, int borderType)
+{
+	cv::blur(*src, *dst, ksize, anchor, borderType);
+}
+
+#endif
