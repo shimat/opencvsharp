fc12369e720bf6cdf9d0f86bff5987e0703b37f1
shimat <schimatk@gmail.com>
2014-02-08 00:46:51 +0900

    Added imgproc methods

--- src/OpenCvSharpExtern/imgproc.h
+++ src/OpenCvSharpExtern/imgproc.h
@@ -60,91 +60,78 @@ CVAPI(void) imgproc_blur(cv::_InputArray *src, cv::_OutputArray *dst, CvSize ksi
 {
 	cv::blur(*src, *dst, ksize, anchor, borderType);
 }
-//! applies non-separable 2D linear filter to the image
+
 CVAPI(void) imgproc_filter2D(cv::_InputArray *src, cv::_OutputArray *dst, int ddepth,
 	cv::_InputArray *kernel, CvPoint anchor, double delta, int borderType)
 {
+	cv::filter2D(*src, *dst, ddepth, *kernel, anchor, delta, borderType);
 }
 
-//! applies separable 2D linear filter to the image
 CVAPI(void) imgproc_sepFilter2D(cv::_InputArray *src, cv::_OutputArray *dst, int ddepth,
 	cv::_InputArray *kernelX, cv::_InputArray kernelY,
 	CvPoint anchor,	double delta, int borderType)
 {
 }
 
-//! applies generalized Sobel operator to the image
 CVAPI(void) imgproc_Sobel(cv::_InputArray *src, cv::_OutputArray *dst, int ddepth,
 	int dx, int dy, int ksize, double scale, double delta, int borderType)
 {
 }
 
-//! applies the vertical or horizontal Scharr operator to the image
 CVAPI(void) imgproc_Scharr(cv::_InputArray *src, cv::_OutputArray *dst, int ddepth,
 	int dx, int dy, double scale, double delta,	int borderType)
 {
 }
 
-//! applies Laplacian operator to the image
 CVAPI(void) imgproc_Laplacian(cv::_InputArray *src, cv::_OutputArray *dst, int ddepth,
 	int ksize, double scale, double delta, int borderType)
 {
 }
 
-//! applies Canny edge detector and produces the edge map.
 CVAPI(void) imgproc_Canny(cv::_InputArray *src, cv::_OutputArray *edges,
 	double threshold1, double threshold2, int apertureSize, bool L2gradient)
 {
 }
 
-//! computes minimum eigen value of 2x2 derivative covariation matrix at each pixel - the cornerness criteria
 CVAPI(void) imgproc_cornerMinEigenVal(cv::_InputArray *src, cv::_OutputArray *dst,
 	int blockSize, int ksize, int borderType)
 {
 }
 
-//! computes Harris cornerness criteria at each image pixel
 CVAPI(void) imgproc_cornerHarris(cv::_InputArray *src, cv::_OutputArray *dst, int blockSize,
 	int ksize, double k, int borderType)
 {
 }
 
-// low-level function for computing eigenvalues and eigenvectors of 2x2 matrices
 CVAPI(void) imgproc_eigen2x2(const float* a, float* e, int n);
 
-//! computes both eigenvalues and the eigenvectors of 2x2 derivative covariation matrix  at each pixel. The output is stored as 6-channel matrix.
 CVAPI(void) imgproc_cornerEigenValsAndVecs(cv::_InputArray *src, cv::_OutputArray *dst,
 	int blockSize, int ksize, int borderType)
 {
 }
 
-//! computes another complex cornerness criteria at each pixel
 CVAPI(void) imgproc_preCornerDetect(cv::_InputArray *src, cv::_OutputArray *dst, int ksize,	int borderType)
 {
 }
 
-//! adjusts the corner locations with sub-pixel accuracy to maximize the certain cornerness criteria
 CVAPI(void) imgproc_cornerSubPix(cv::_InputArray *src, cv::_OutputArray *corners,
 	CvSize winSize, CvSize zeroZone, CvTermCriteria criteria)
 {
 }
 
-//! finds the strong enough corners where the cornerMinEigenVal() or cornerHarris() report the local maxima
 CVAPI(void) imgproc_goodFeaturesToTrack(cv::_InputArray *src, cv::_OutputArray *corners,
 	int maxCorners, double qualityLevel, double minDistance,
 	cv::_InputArray *mask, int blockSize, bool useHarrisDetector, double k)
 {
 }
 
-//! finds lines in the black-n-white image using the standard or pyramid Hough transform
-CVAPI(void) imgproc_HoughLines(cv::_InputArray *src, cv::_OutputArray *lines,
+CVAPI(void) imgproc_HoughLines(cv::_InputArray *src, std::vector<cv::Vec2f> *lines,
 	double rho, double theta, int threshold,
 	double srn, double stn)
 {
 	cv::HoughLines(*src, *lines, rho, theta, threshold, srn, stn);
 }
 
-//! finds line segments in the black-n-white image using probabilistic Hough transform
 CVAPI(void) imgproc_HoughLinesP(cv::_InputArray *src, std::vector<cv::Vec4i> *lines,
 	double rho, double theta, int threshold,
 	double minLineLength, double maxLineGap)
@@ -152,8 +139,7 @@ CVAPI(void) imgproc_HoughLinesP(cv::_InputArray *src, std::vector<cv::Vec4i> *li
 	cv::HoughLinesP(*src, *lines, rho, theta, threshold, minLineLength, maxLineGap);
 }
 
-//! finds circles in the grayscale image using 2+1 gradient Hough transform
-CVAPI(void) imgproc_HoughCircles(cv::_InputArray *src, cv::_OutputArray *circles,
+CVAPI(void) imgproc_HoughCircles(cv::_InputArray *src, std::vector<cv::Vec3f> *circles,
 	int method, double dp, double minDist,
 	double param1, double param2, int minRadius, int maxRadius)
 {
