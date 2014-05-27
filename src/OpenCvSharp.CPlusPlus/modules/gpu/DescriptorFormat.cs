3fb14aa55080d63d7e6478bb5c245605a1574d20
shimat <schimatk@gmail.com>
2014-05-27 21:51:27 +0900

    Fixed merge mistake

---
+++ src/OpenCvSharp.CPlusPlus/modules/gpu/DescriptorFormat.cs
@@ -0,0 +1,31 @@
+﻿/*
+* (C) 2008-2014 shimat
+* This code is licenced under the LGPL.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenCvSharp.CPlusPlus.Gpu
+{
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public enum DescriptorFormat 
+    {
+        /// <summary>
+        /// 
+        /// [HOGDescriptor::DESCR_FORMAT_ROW_BY_ROW]
+        /// </summary>
+        RowByRow = 0,
+
+        /// <summary>
+        /// 
+        /// [HOGDescriptor::DESCR_FORMAT_COL_BY_COL]
+        /// </summary>
+        ColByCol = 1,
+    }
+}
+
+
