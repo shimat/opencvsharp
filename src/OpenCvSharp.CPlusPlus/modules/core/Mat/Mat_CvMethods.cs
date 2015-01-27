bab108cf525220a5227e3546eaa7c6a9e7a8d42f
shimat <schimatk@gmail.com>
2015-01-27 19:26:40 +0900

    Mat.PyrUp calls PyrDown

--- src/OpenCvSharp.CPlusPlus/modules/core/Mat/Mat_CvMethods.cs
+++ src/OpenCvSharp.CPlusPlus/modules/core/Mat/Mat_CvMethods.cs
@@ -1755,7 +1755,7 @@ namespace OpenCvSharp.CPlusPlus
         public Mat PyrUp(Size? dstSize = null, BorderType borderType = BorderType.Default)
         {
             var dst = new Mat();
-            Cv2.PyrDown(this, dst, dstSize, borderType);
+            Cv2.PyrUp(this, dst, dstSize, borderType);
             return dst;
         }
 
