c5558ef9aef4ca4abc2b944c6988f0f22da992fa
KojiKobayashi <k.kobayashi@33i.co.jp>
2015-05-16 16:02:00 +0900

    add a sample of Nick binarization.

--- sample/CStyleSamplesCS/Samples/BinarizationMethods.cs
+++ sample/CStyleSamplesCS/Samples/BinarizationMethods.cs
@@ -23,6 +23,7 @@ namespace CStyleSamplesCS
             using (var imgNiblack = new IplImage(imgSrc.Size, BitDepth.U8, 1))
             using (var imgSauvola = new IplImage(imgSrc.Size, BitDepth.U8, 1))
             using (var imgBernsen = new IplImage(imgSrc.Size, BitDepth.U8, 1))
+            using (var imgNick = new IplImage(imgSrc.Size, BitDepth.U8, 1))
             {
                 //Cv.Smooth(imgSrc, imgGauss, SmoothType.Gaussian, 9);
                 //Cv.EqualizeHist(imgGauss, imgGauss);
@@ -30,14 +31,14 @@ namespace CStyleSamplesCS
                 Stopwatch sw = Stopwatch.StartNew();
                 {
                     const int Size = 31;
-                    const double K = 0.2;
+                    const double K = -0.2;
                     Binarizer.NiblackFast(imgGauss, imgNiblack, Size, K);
                 }
                 Console.WriteLine("Niblack: {0}ms", sw.ElapsedMilliseconds);
                 sw.Reset(); sw.Start();
                 {
                     const int Size = 31;
-                    const double K = 0.9;
+                    const double K = 0.2;
                     const double R = 32;
                     Binarizer.SauvolaFast(imgGauss, imgSauvola, Size, K, R);
                 }
@@ -50,12 +51,20 @@ namespace CStyleSamplesCS
                     Binarizer.Bernsen(imgGauss, imgBernsen, Size, ContrastMin, BgThreshold);
                 }
                 Console.WriteLine("bernsen: {0}ms", sw.ElapsedMilliseconds);
+                sw.Reset(); sw.Start();
+                {
+                    const int Size = 31;
+                    const double K = -0.14;
+                    Binarizer.Nick(imgGauss, imgNick, Size, K);
+                }
+                Console.WriteLine("nick: {0}ms", sw.ElapsedMilliseconds);
 
                 using (new CvWindow("src", imgSrc))
                 //using (new CvWindow("gauss", imgGauss))
                 using (new CvWindow("niblack", imgNiblack))
                 using (new CvWindow("sauvola", imgSauvola))
                 using (new CvWindow("bernsen", imgBernsen))
+                using (new CvWindow("nick", imgNick))
                 {
                     Cv.WaitKey();
                 }
