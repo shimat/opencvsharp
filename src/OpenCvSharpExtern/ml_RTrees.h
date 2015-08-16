5e94c1783b1ea1218492e48cd250703a80570a92
shimat <schimatk@gmail.com>
2015-08-16 22:59:46 +0900

    Added RTrees

---
+++ src/OpenCvSharpExtern/ml_RTrees.h
@@ -0,0 +1,56 @@
+#ifndef _CPP_ML_RTREES_H_
+#define _CPP_ML_RTREES_H_
+
+#include "include_opencv.h"
+using namespace cv::ml;
+
+
+CVAPI(int) ml_RTrees_getCalculateVarImportance(RTrees *obj)
+{
+	return obj->getCalculateVarImportance() ? 1 : 0;
+}
+CVAPI(void) ml_RTrees_setCalculateVarImportance(RTrees *obj, int val)
+{
+	obj->setCalculateVarImportance(val != 0);
+}
+
+CVAPI(int) ml_RTrees_getActiveVarCount(RTrees *obj)
+{
+	return obj->getActiveVarCount();
+}
+CVAPI(void) ml_RTrees_setActiveVarCount(RTrees *obj, int val)
+{
+	obj->setActiveVarCount(val);
+}
+
+CVAPI(MyCvTermCriteria) ml_RTrees_getTermCriteria(RTrees *obj)
+{
+	return c(obj->getTermCriteria());
+}
+CVAPI(void) ml_RTrees_setTermCriteria(RTrees *obj, MyCvTermCriteria val)
+{
+	obj->setTermCriteria(cpp(val));
+}
+
+CVAPI(cv::Mat*) ml_RTrees_getVarImportance(RTrees *obj)
+{
+	return new cv::Mat(obj->getVarImportance());
+}
+
+CVAPI(cv::Ptr<RTrees>*) ml_RTrees_create()
+{
+	cv::Ptr<RTrees> ptr = RTrees::create();
+	return new cv::Ptr<RTrees>(ptr);
+}
+
+CVAPI(void) ml_Ptr_RTrees_delete(cv::Ptr<RTrees> *obj)
+{
+	delete obj;
+}
+
+CVAPI(RTrees*) ml_Ptr_RTrees_get(cv::Ptr<RTrees> *obj)
+{
+	return obj->get();
+}
+
+#endif
