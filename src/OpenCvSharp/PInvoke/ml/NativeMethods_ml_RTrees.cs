1dc4a5d7cad451d6f892849a4ac8d93c811e0e07
shimat <schimatk@gmail.com>
2015-08-16 22:59:46 +0900

    Added RTrees

--- src/OpenCvSharp/PInvoke/ml/NativeMethods_ml_RTrees.cs
+++ src/OpenCvSharp/PInvoke/ml/NativeMethods_ml_RTrees.cs
@@ -7,6 +7,31 @@ namespace OpenCvSharp
 {
     static partial class NativeMethods
     {
-        // TODO
+        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int ml_RTrees_getCalculateVarImportance(IntPtr obj);
+        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void ml_RTrees_setCalculateVarImportance(IntPtr obj, int val);
+        
+        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int ml_RTrees_getActiveVarCount(IntPtr obj);
+        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void ml_RTrees_setActiveVarCount(IntPtr obj, int val);
+        
+        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
+        public static extern TermCriteria ml_RTrees_getTermCriteria(IntPtr obj);
+        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void ml_RTrees_setTermCriteria(IntPtr obj, TermCriteria val);
+        
+        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr ml_RTrees_getVarImportance(IntPtr obj);
+        
+        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr ml_RTrees_create();
+        
+        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void ml_Ptr_RTrees_delete(IntPtr obj);
+        
+        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr ml_Ptr_RTrees_get(IntPtr obj);
     }
 }
--- src/OpenCvSharp/modules/ml/RTrees.cs
+++ src/OpenCvSharp/modules/ml/RTrees.cs
@@ -9,7 +9,7 @@ namespace OpenCvSharp.ML
     /// </summary>
 #else
 	/// <summary>
-    /// Random Trees
+    /// The class implements the random forest predictor.
     /// </summary>
 #endif
 	public class RTrees : DTrees
@@ -18,21 +18,29 @@ namespace OpenCvSharp.ML
         /// Track whether Dispose has been called
         /// </summary>
         private bool disposed;
+        private Ptr<RTrees> ptrObj;
 
 		#region Init and Disposal
-#if LANG_JP
+
         /// <summary>
-        /// 既定のコンストラクタ
+        /// Creates instance by raw pointer cv::ml::Boost*
         /// </summary>
-#else
-		/// <summary>
-        /// Default constructor
-        /// </summary>
-#endif
-        public RTrees()
+        protected RTrees(IntPtr p)
+            : base()
         {
+            ptrObj = new Ptr<RTrees>(p);
+            ptr = ptrObj.Get();
         }
 
+        /// <summary>
+        /// Creates the empty model.
+        /// </summary>
+        /// <returns></returns>
+        public static new RTrees Create()
+	    {
+            IntPtr ptr = NativeMethods.ml_RTrees_create();
+            return new RTrees(ptr);
+	    }
 #if LANG_JP
         /// <summary>
         /// リソースの解放
@@ -58,11 +66,13 @@ namespace OpenCvSharp.ML
                 {
                     if (disposing)
                     {
+                        if (ptrObj != null)
+                        {
+                            ptrObj.Dispose();
+                            ptrObj = null;
+                        }
                     }
-                    if (IsEnabledDispose)
-                    {
-                        ptr = IntPtr.Zero;
-                    }
+                    ptr = IntPtr.Zero;
                     disposed = true;
                 }
                 finally
@@ -74,10 +84,56 @@ namespace OpenCvSharp.ML
         #endregion
 
 		#region Properties
+
+        /// <summary>
+        /// If true then variable importance will be calculated and then 
+        /// it can be retrieved by RTrees::getVarImportance. Default value is false.
+        /// </summary>
+        public bool CalculateVarImportance
+        {
+            get { return NativeMethods.ml_RTrees_getCalculateVarImportance(ptr) != 0; }
+            set { NativeMethods.ml_RTrees_setCalculateVarImportance(ptr, value ? 1 : 0); }
+        }
+
+        /// <summary>
+        /// The size of the randomly selected subset of features at each tree node 
+        /// and that are used to find the best split(s).
+        /// </summary>
+        public bool ActiveVarCount
+        {
+            get { return NativeMethods.ml_RTrees_getActiveVarCount(ptr) != 0; }
+            set { NativeMethods.ml_RTrees_setActiveVarCount(ptr, value ? 1 : 0); }
+        }
+
+        /// <summary>
+        /// The termination criteria that specifies when the training algorithm stops.
+        /// </summary>
+        public TermCriteria TermCriteria
+        {
+            get { return NativeMethods.ml_RTrees_getTermCriteria(ptr); }
+            set { NativeMethods.ml_RTrees_setTermCriteria(ptr, value); }
+        }
+
         #endregion
 
 		#region Methods
 		
+        /// <summary>
+        /// Returns the variable importance array. 
+        /// The method returns the variable importance vector, computed at the training 
+        /// stage when CalculateVarImportance is set to true. If this flag was set to false, 
+        /// the empty matrix is returned.
+        /// </summary>
+        /// <returns></returns>
+	    public Mat GetVarImportance()
+	    {
+	        if (disposed)
+	            throw new NotImplementedException(GetType().Name);
+
+            IntPtr p = NativeMethods.ml_RTrees_getVarImportance(ptr);
+            return new Mat(p);
+	    }
+
         #endregion
 	}
 }
