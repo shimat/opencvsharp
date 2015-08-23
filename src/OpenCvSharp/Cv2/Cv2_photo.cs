9564ad30a579194fc7e6a8b22ffd28589c832c30
shimat <schimatk@gmail.com>
2015-08-23 17:40:16 +0900

    Added DenoiseTVL1

--- src/OpenCvSharp/Cv2/Cv2_photo.cs
+++ src/OpenCvSharp/Cv2/Cv2_photo.cs
@@ -201,5 +201,37 @@ namespace OpenCvSharp
                 h, hColor, templateWindowSize, searchWindowSize);
         }
         #endregion
+
+        /// <summary>
+        /// Primal-dual algorithm is an algorithm for solving special types of variational problems 
+        /// (that is, finding a function to minimize some functional). As the image denoising, 
+        /// in particular, may be seen as the variational problem, primal-dual algorithm then 
+        /// can be used to perform denoising and this is exactly what is implemented.
+        /// </summary>
+        /// <param name="observations">This array should contain one or more noised versions 
+        /// of the image that is to be restored.</param>
+        /// <param name="result">Here the denoised image will be stored. There is no need to 
+        /// do pre-allocation of storage space, as it will be automatically allocated, if necessary.</param>
+        /// <param name="lambda">Corresponds to \f$\lambda\f$ in the formulas above. 
+        /// As it is enlarged, the smooth (blurred) images are treated more favorably than 
+        /// detailed (but maybe more noised) ones. Roughly speaking, as it becomes smaller, 
+        /// the result will be more blur but more sever outliers will be removed.</param>
+        /// <param name="niters"> Number of iterations that the algorithm will run. 
+        /// Of course, as more iterations as better, but it is hard to quantitatively 
+        /// refine this statement, so just use the default and increase it if the results are poor.</param>
+// ReSharper disable once InconsistentNaming
+        public static void DenoiseTVL1(
+            IEnumerable<Mat> observations, Mat result, double lambda = 1.0, int niters = 30)
+        {
+            if (observations == null)
+                throw new ArgumentNullException("observations");
+            if (result == null) 
+                throw new ArgumentNullException("result");
+
+            IntPtr[] observationsPtrs = EnumerableEx.SelectPtrs(observations);
+            NativeMethods.photo_denoise_TVL1(observationsPtrs, observationsPtrs.Length, result.CvPtr, lambda, niters);
+            GC.KeepAlive(observations);
+            GC.KeepAlive(result);
+        }
     }
 }
