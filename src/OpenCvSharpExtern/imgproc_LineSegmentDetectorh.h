fedbd8cde81f5d5ab467ff6a67ef578f3186d5d2
shimat <schimatk@gmail.com>
2015-08-27 19:01:37 +0900

    Added LineSegmentDetector

---
+++ src/OpenCvSharpExtern/imgproc_LineSegmentDetectorh.h
@@ -0,0 +1,43 @@
+#ifndef _CPP_IMGPROC_LINESEGMENTDETECTOR_H_
+#define _CPP_IMGPROC_LINESEGMENTDETECTOR_H_
+
+#include "include_opencv.h"
+
+
+CVAPI(void) imgproc_LineSegmentDetector_detect(cv::LineSegmentDetector *obj, cv::_InputArray *image, cv::_OutputArray *lines,
+    cv::_OutputArray *width, cv::_OutputArray *prec, cv::_OutputArray *nfa)
+{
+    obj->detect(*image, *lines, entity(width), entity(prec), entity(nfa));
+}
+
+CVAPI(void) imgproc_LineSegmentDetector_drawSegments(cv::LineSegmentDetector *obj, cv::_InputOutputArray *image, cv::_InputArray *lines)
+{
+    obj->drawSegments(*image, *lines);
+}
+
+CVAPI(int) imgproc_LineSegmentDetector_compareSegments(cv::LineSegmentDetector *obj, MyCvSize size,
+    cv::_InputArray *lines1, cv::_InputArray *lines2, cv::_InputOutputArray *image)
+{
+    return obj->compareSegments(cpp(size), *lines1, *lines2, entity(image));
+}
+
+
+CVAPI(cv::Ptr<cv::LineSegmentDetector>*) imgproc_createLineSegmentDetector(
+    int refine, double scale, double sigma_scale, double quant, double ang_th,
+    double log_eps, double density_th, int n_bins)
+{
+    return clone( cv::createLineSegmentDetector(
+        refine, scale, sigma_scale, quant, ang_th, log_eps, density_th, n_bins));
+}
+
+CVAPI(void) imgproc_Ptr_LineSegmentDetector_delete(cv::Ptr<cv::LineSegmentDetector> *obj)
+{
+    delete obj;
+}
+
+CVAPI(cv::LineSegmentDetector*) imgproc_Ptr_LineSegmentDetector_get(cv::Ptr<cv::LineSegmentDetector> *obj)
+{
+    return obj->get();
+}
+
+#endif
