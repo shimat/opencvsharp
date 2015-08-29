b80f173f56f1e5da1cbd5ba2a96fd2766fbda32a
shimat <schimatk@gmail.com>
2015-08-29 18:42:00 +0900

    Added GeneralizedHough

--- src/OpenCvSharpExtern/imgproc.cpp
+++ src/OpenCvSharpExtern/imgproc.cpp
@@ -2,4 +2,5 @@
 #include "imgproc_Subdiv2D.h"
 #include "imgproc_CLAHE.h"
 #include "imgproc_LineIterator.h"
-#include "imgproc_LineSegmentDetectorh.h"
+#include "imgproc_LineSegmentDetector.h"
+#include "imgproc_GeneralizedHough.h"
