048def2253950c2d30f2e7991325dba0d3bf0d18
shimat <schimatk@gmail.com>
2015-10-23 16:23:59 +0900

    bugfix bugfix

--- src/OpenCvSharp/modules/core/Struct/Rectf.cs
+++ src/OpenCvSharp/modules/core/Struct/Rectf.cs
@@ -85,8 +85,8 @@ namespace OpenCvSharp
             {
                 X = left,
                 Y = top,
-                Width = right - left - 1,
-                Height = bottom - top - 1
+                Width = right - left + 1,
+                Height = bottom - top + 1
             };
 
             if (r.Width < 0)
