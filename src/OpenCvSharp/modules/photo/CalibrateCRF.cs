e789c7efb900bb7e9af1ce493a2609ce54094bc7
shimat <schimatk@gmail.com>
2015-10-26 13:48:29 +0900

    Added the first edition of HDR wrapper

---
+++ src/OpenCvSharp/modules/photo/CalibrateCRF.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenCvSharp.Util;
+
+namespace OpenCvSharp
+{
+    /// <summary>
+    /// The base class for camera response calibration algorithms.
+    /// </summary>
+    public abstract class CalibrateCRF : Algorithm
+    {
+        /// <summary>
+        /// Recovers inverse camera response.
+        /// </summary>
+        /// <param name="src">vector of input images</param>
+        /// <param name="dst">256x1 matrix with inverse camera response function</param>
+        /// <param name="times">vector of exposure time values for each image</param>
+        public virtual void Process(IEnumerable<Mat> src, OutputArray dst, IEnumerable<float> times)
+        {
+            if (src == null)
+                throw new ArgumentNullException("src");
+            if (dst == null)
+                throw new ArgumentNullException("dst");
+            if (times == null)
+                throw new ArgumentNullException("times");
+            dst.ThrowIfNotReady();
+            
+            IntPtr[] srcArray = EnumerableEx.SelectPtrs(src);
+            float[] timesArray = EnumerableEx.ToArray(times);
+            if (srcArray.Length != timesArray.Length)
+                throw new OpenCvSharpException("src.Count() != times.Count");
+
+            NativeMethods.photo_CalibrateCRF_process(ptr, srcArray, srcArray.Length, dst.CvPtr, timesArray);
+
+            dst.Fix();
+            GC.KeepAlive(src);
+        }
+    }
+}
--- src/OpenCvSharpExtern/photo_HDR.h
+++ src/OpenCvSharpExtern/photo_HDR.h
@@ -3,6 +3,52 @@
 
 #include "include_opencv.h"
 
-// TODO
+CVAPI(cv::Ptr<cv::CalibrateDebevec>*) photo_createCalibrateDebevec(
+    int samples, float lambda, int random) 
+{
+    return clone(cv::createCalibrateDebevec(samples, lambda, random != 0));
+}
+
+CVAPI(cv::Ptr<cv::CalibrateRobertson>*) photo_createCalibrateRobertson(
+    int max_iter, float threshold)
+{
+    return clone(cv::createCalibrateRobertson(max_iter, threshold));
+}
+
+CVAPI(void) photo_Ptr_CalibrateDebevec_delete(cv::Ptr<cv::CalibrateDebevec> *obj)
+{
+    delete obj;
+}
+CVAPI(void) photo_Ptr_CalibrateRobertson_delete(cv::Ptr<cv::CalibrateRobertson> *obj)
+{
+    delete obj;
+}
+
+CVAPI(cv::CalibrateDebevec*) photo_Ptr_CalibrateDebevec_get(cv::Ptr<cv::CalibrateDebevec> *obj)
+{
+    return obj->get();
+}
+CVAPI(cv::CalibrateRobertson*) photo_Ptr_CalibrateRobertson_get(cv::Ptr<cv::CalibrateRobertson> *obj)
+{
+    return obj->get();
+}
+
+CVAPI(void) photo_CalibrateCRF_process(
+    cv::CalibrateCRF *obj, 
+    cv::Mat ** srcImgs, int srcImgsLength, cv::_OutputArray *dst, float* times)
+{
+    // Build Mat Vector of images
+    std::vector<cv::Mat> srcImgsVec(srcImgsLength);
+
+    // Build float Vector of times
+    std::vector<float> times_vec(srcImgsLength);
+    
+    for (int i = 0; i < srcImgsLength; i++) {
+        srcImgsVec[i] = *srcImgs[i];
+        times_vec[i] = times[i];
+    }
+
+    obj->process(srcImgsVec, *dst, times_vec);
+}
 
 #endif
