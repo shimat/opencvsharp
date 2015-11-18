b13aa4c45d4a06b23c748abb4ad4b8257fb1a8c1
shimat <schimatk@gmail.com>
2015-11-18 23:34:58 +0900

    Fixed BlobRenderer when mode != color

--- src/OpenCvSharp.Blob/BlobRenderer.cs
+++ src/OpenCvSharp.Blob/BlobRenderer.cs
@@ -140,7 +140,10 @@ namespace OpenCvSharp.Blob
 
             foreach (var kv in blobs)
             {
-                PerformOne(blobs.Labels, kv.Value, imgSrc, imgDst, mode, palette[kv.Key], alpha);
+                Scalar color = default (Scalar);
+                if (palette.ContainsKey(kv.Key))
+                    color = palette[kv.Key];
+                PerformOne(blobs.Labels, kv.Value, imgSrc, imgDst, mode, color, alpha);
             }
         }
 
