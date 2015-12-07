d43e97d1f1b7bb461bdae9b419c6f42fe05e93ff
shimat <schimatk@gmail.com>
2015-12-07 12:01:07 +0900

    added groupRectangles

--- src/OpenCvSharp/PInvoke/NativeMethods_objdetect.cs
+++ src/OpenCvSharp/PInvoke/NativeMethods_objdetect.cs
@@ -183,5 +183,23 @@ namespace OpenCvSharp
         [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
         public static extern void objdetect_HOGDescriptor_nlevels_set(IntPtr self, int value);
         #endregion
+
+        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void objdetect_groupRectangles1(IntPtr rectList, int groupThreshold, double eps);
+
+        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void objdetect_groupRectangles2(IntPtr rectList, IntPtr weights, int groupThreshold, double eps);
+
+        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void objdetect_groupRectangles3(
+            IntPtr rectList, int groupThreshold, double eps, IntPtr weights, IntPtr levelWeights);
+
+        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void objdetect_groupRectangles4(
+            IntPtr rectList, IntPtr rejectLevels, IntPtr levelWeights, int groupThreshold, double eps);
+
+        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void objdetect_groupRectangles_meanshift(
+            IntPtr rectList, IntPtr foundWeights, IntPtr foundScales, double detectThreshold, Size winDetSize);
     }
 }
