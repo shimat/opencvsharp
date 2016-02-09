8c143e5124dfb4fd59f2098a8c95947b3b0419d9
shimat <schimatk@gmail.com>
2016-02-09 11:11:22 +0900

    Added DebuggerVisualizers for VS2015 (by @fdncred pull request)

---
+++ src/OpenCvSharp.DebuggerVisualizers2015/MatDebuggerVisualizer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Microsoft.VisualStudio.DebuggerVisualizers;
+
+namespace OpenCvSharp.DebuggerVisualizers2015
+{
+    /// <summary>
+    /// ビジュアライザでの視覚化処理
+    /// </summary>
+    public class MatDebuggerVisualizer : DialogDebuggerVisualizer
+    {
+        protected override void Show(IDialogVisualizerService windowService, IVisualizerObjectProvider objectProvider)
+        {
+            // MatProxyが送られてくるはず
+            using (var proxy = objectProvider.GetObject() as MatProxy)
+            {
+                if (proxy == null)
+                {
+                    throw new ArgumentException();
+                }
+                // Formに表示
+                using (var form = new ImageViewer(proxy))
+                {
+                    windowService.ShowDialog(form);
+                }
+            } 
+        }
+    }
+    
+    /// <summary>
+    /// シリアライズ処理
+    /// </summary>
+    public class MatObjectSource : VisualizerObjectSource
+    {
+        public override void GetData(object target, Stream outgoingData)
+        {
+            var bf = new BinaryFormatter();
+            bf.Serialize(outgoingData, new MatProxy((Mat)target));
+        }
+    }
+}
