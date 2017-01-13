640a6de29e2c8270a073a5af5d63aae9dfd0cfe8
shimat <schimatk@gmail.com>
2017-01-13 19:19:06 +0900

    moved tests

---
+++ test/OpenCvSharp.Tests/InvokeTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace OpenCvSharp.Tests
+{
+    [TestFixture]
+    public class InvokeTest
+    {
+        [Test, Ignore("")]
+        public void ExistsAllEntryPoints()
+        {
+            var type = typeof (NativeMethods);
+#if net20 || net40
+            var methods = type.GetMethods(BindingFlags.Public | BindingFlags.Static);
+#else
+            var methods = type.GetTypeInfo().GetMethods(BindingFlags.Public | BindingFlags.Static);
+#endif 
+            foreach (var method in methods)
+            {
+                var parameters = method.GetParameters();
+                var values = new object[parameters.Length];
+
+                for (int i = 0; i < parameters.Length; i++)
+                {
+                    var paramType = parameters[i].ParameterType;
+                    if (paramType == typeof (string))
+                        values[i] = "";
+                    else
+                        values[i] = Activator.CreateInstance(parameters[i].ParameterType);
+                }
+
+                /*
+                var task = Task.Factory.StartNew(
+                    () =>
+                    {
+                        try
+                        {
+                            Console.WriteLine(method.Name);
+                            method.Invoke(null, values);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Caught one (inside):" + e.GetType().Name);
+                        }
+                    });
+                try
+                {
+                    task.Wait(1000);
+                }
+                catch (AggregateException ae)
+                {
+                    // Assume we know what's going on with this particular exception. 
+                    // Rethrow anything else. AggregateException.Handle provides 
+                    // another way to express this. See later example. 
+                    foreach (var e in ae.InnerExceptions)
+                    {
+                        if (e is TargetInvocationException)
+                        {
+                            Console.WriteLine("After:" + e.GetType().Name + e.StackTrace);
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                }
+                */
+
+                ///* TODO
+                try
+                {
+                    method.Invoke(null, values);
+                }
+                catch (TargetInvocationException ex)
+                {
+                    ex.ToString();
+                }
+                catch (MissingMethodException ex)
+                {
+                    Assert.Fail(ex.ToString());
+                }
+                catch (Exception ex)
+                {
+                    ex.ToString();
+                }
+                //*/
+            }
+
+        }
+    }
+}
+
