160b5f12dfa735e8882f4dde0347e3f175fb82ee
shimat <schimatk@gmail.com>
2017-01-13 19:19:06 +0900

    moved tests

---
+++ test/OpenCvSharp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OpenCvSharp.Tests")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ffbbcf99-97f0-4f81-aaf6-8d851a8e1d2e")]
---
+++ test/OpenCvSharp.Tests/core/RNGTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace OpenCvSharp.Tests
+{
+    [TestFixture]
+    public class RNGTest
+    {
+        [Test]
+        public void Next()
+        {
+            var rng = new RNG(0xffffffff);
+
+            Assert.AreEqual(130063606, rng.Next());
+            Assert.AreEqual(3003295397, rng.Next());
+            Assert.AreEqual(3870020839, rng.Next());
+        }
+
+        [Test]
+        public void Uniform()
+        {
+            var rng = new RNG(1234);
+
+            Assert.AreEqual(4, rng.Uniform(0, 10));
+            Assert.AreEqual(6, rng.Uniform(0, 10));
+            Assert.AreEqual(9, rng.Uniform(0, 10));
+        }
+    }
+}
