d6847ccb40f8f04abfe5bee1a9f697e4aeb89b7c
shimat <schimatk@gmail.com>
2017-01-14 18:56:47 +0900

    changed directory structure

--- src/OpenCvSharp/PInvoke/face/NativeMethods_face.cs
+++ src/OpenCvSharp/PInvoke/face/NativeMethods_face.cs
@@ -11,54 +11,15 @@ namespace OpenCvSharp
     static partial class NativeMethods
     {
         [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void face_FaceRecognizer_delete(IntPtr obj);
-        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void face_FaceRecognizer_train(
-        IntPtr obj, IntPtr[] src, int srcLength, int[] labels, int labelsLength);
-        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void face_FaceRecognizer_update(
-        IntPtr obj, IntPtr[] src, int srcLength, int[] labels, int labelsLength);
-        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
-        public static extern int face_FaceRecognizer_predict1(IntPtr obj, IntPtr src);
-        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void face_FaceRecognizer_predict2(
-        IntPtr obj, IntPtr src, out int label, out double confidence);
-        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void face_FaceRecognizer_save1(IntPtr obj, string filename);
-        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void face_FaceRecognizer_load1(IntPtr obj, string filename);
-        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void face_FaceRecognizer_save2(IntPtr obj, IntPtr fs);
-        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void face_FaceRecognizer_load2(IntPtr obj, IntPtr fs);
-        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
-        public static extern IntPtr face_FaceRecognizer_info(IntPtr obj);
-        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr face_createEigenFaceRecognizer(
-        int numComponents, double threshold);
+            int numComponents, double threshold);
+
         [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr face_createFisherFaceRecognizer(
-        int numComponents, double threshold);
+            int numComponents, double threshold);
+
         [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr face_createLBPHFaceRecognizer(
-        int radius, int neighbors, int gridX, int gridY, double threshold);
-
-        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void face_FaceRecognizer_setLabelInfo(IntPtr obj, int label,
-            [MarshalAs(UnmanagedType.LPStr)] string strInfo);
-        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void face_FaceRecognizer_getLabelInfo(IntPtr obj, int label, IntPtr dst);
-        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void face_FaceRecognizer_getLabelsByString(IntPtr obj,
-            [MarshalAs(UnmanagedType.LPStr)] string str, IntPtr dst);
-        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
-        public static extern double face_FaceRecognizer_getThreshold(IntPtr obj);
-        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void face_FaceRecognizer_setThreshold(IntPtr obj, double val);
-
-        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
-        public static extern IntPtr face_Ptr_FaceRecognizer_get(IntPtr obj);
-        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void face_Ptr_FaceRecognizer_delete(IntPtr obj);
+            int radius, int neighbors, int gridX, int gridY, double threshold);
     }
 }
--- src/OpenCvSharpExtern/face_BasicFaceRecognizer.h
+++ src/OpenCvSharpExtern/face_BasicFaceRecognizer.h
@@ -1,8 +1,58 @@
 #ifndef _CPP_FACE_BASICFACERECOGNIZER_H_
 #define _CPP_FACE_BASICFACERECOGNIZER_H_
 
+// ReSharper disable CppNonInlineFunctionDefinitionInHeaderFile
+
 #include "include_opencv.h"
 using namespace cv::face;
+
+CVAPI(int) face_BasicFaceRecognizer_getNumComponents(BasicFaceRecognizer *obj)
+{
+    return obj->getNumComponents();
+}
+CVAPI(void) face_BasicFaceRecognizer_setNumComponents(BasicFaceRecognizer *obj, int val)
+{
+    obj->getNumComponents();
+}
+
+CVAPI(double) face_BasicFaceRecognizer_getThreshold(BasicFaceRecognizer *obj)
+{
+    return obj->getThreshold();
+}
+CVAPI(void) face_BasicFaceRecognizer_setThreshold(BasicFaceRecognizer *obj, double val)
+{
+    obj->setThreshold(val);
+}
+CVAPI(void) face_BasicFaceRecognizer_getProjections(BasicFaceRecognizer *obj, std::vector<cv::Mat> *dst)
+{
+    std::vector<cv::Mat> result = obj->getProjections();
+    dst->clear();
+    dst->reserve(result.size());
+    for (size_t i = 0; i < result.size(); i++)
+    {
+        dst->push_back(result[i]);
+    }
+}
+CVAPI(void) face_BasicFaceRecognizer_getLabels(BasicFaceRecognizer *obj, cv::Mat *dst)
+{
+    cv::Mat result = obj->getLabels();
+    result.copyTo(*dst);
+}
+CVAPI(void) face_BasicFaceRecognizer_getEigenValues(BasicFaceRecognizer *obj, cv::Mat *dst)
+{
+    cv::Mat result = obj->getEigenValues();
+    result.copyTo(*dst);
+}
+CVAPI(void) face_BasicFaceRecognizer_getEigenVectors(BasicFaceRecognizer *obj, cv::Mat *dst)
+{
+    cv::Mat result = obj->getEigenVectors();
+    result.copyTo(*dst);
+}
+CVAPI(void) face_BasicFaceRecognizer_getMean(BasicFaceRecognizer *obj, cv::Mat *dst)
+{
+    cv::Mat result = obj->getMean();
+    result.copyTo(*dst);
+}
 
 CVAPI(cv::Ptr<BasicFaceRecognizer>*) face_createEigenFaceRecognizer(int numComponents, double threshold)
 {
