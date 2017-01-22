93a9903763741eb6c46cf2caf61faa78cea2bd22
shimat <schimatk@gmail.com>
2017-01-22 19:34:24 +0900

    added simple BOWKMeansTrainer test

---
+++ test/OpenCvSharp.Tests/face/LBPHFaceRecognizerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace OpenCvSharp.Tests.Face
+{
+    [TestFixture]
+    public class LBPHFaceRecognizerTest
+    {
+        [Test]
+        public void New()
+        {
+            var recognizer = OpenCvSharp.Face.FaceRecognizer.CreateLBPHFaceRecognizer(1, 8, 8, 8, 123);
+            recognizer.Dispose();
+        }
+    }
+}
+
