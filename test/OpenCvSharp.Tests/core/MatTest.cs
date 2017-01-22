d700bd741b0f2f896f806bfd793f03ccb43df634
shimat <schimatk@gmail.com>
2017-01-22 17:52:36 +0900

    fix bugs in MatOfDouble.FromArray and MatOfShort.FromArray

---
+++ test/OpenCvSharp.Tests/core/MatTest.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace OpenCvSharp.Tests
+{
+    [TestFixture]
+    public class MatTest
+    {
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+        }
+
+        [Test]
+        public void MatOfDoubleFromArray()
+        {
+            var array = new double[] {7, 8, 9};
+            var m = MatOfDouble.FromArray(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Assert.That(m.Get<double>(i), Is.EqualTo(array[i]).Within(1e-6));
+            }
+        }
+
+        [Test]
+        public void MatOfFloatFromArray()
+        {
+            var array = new float[] { 7, 8, 9 };
+            var m = MatOfFloat.FromArray(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Assert.That(m.Get<float>(i), Is.EqualTo(array[i]).Within(1e-6));
+            }
+        }
+
+        [Test]
+        public void MatOfIntFromArray()
+        {
+            var array = new int[] { 7, 8, 9 };
+            var m = MatOfInt.FromArray(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Assert.That(m.Get<int>(i), Is.EqualTo(array[i]));
+            }
+        }
+
+        [Test]
+        public void MatOfUShortFromArray()
+        {
+            var array = new ushort[] { 7, 8, 9 };
+            var m = MatOfUShort.FromArray(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Assert.That(m.Get<ushort>(i), Is.EqualTo(array[i]));
+            }
+        }
+
+        [Test]
+        public void MatOfShortFromArray()
+        {
+            var array = new short[] { 7, 8, 9 };
+            var m = MatOfShort.FromArray(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Assert.That(m.Get<short>(i), Is.EqualTo(array[i]));
+            }
+        }
+
+        [Test]
+        public void MatOfByteFromArray()
+        {
+            var array = new byte[] { 7, 8, 9 };
+            var m = MatOfByte.FromArray(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Assert.That(m.Get<byte>(i), Is.EqualTo(array[i]));
+            }
+        }
+    }
+}
+
