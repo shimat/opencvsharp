23324a82919c567e3ed64a9c9182309af3e4cdcf
shimat <schimatk@gmail.com>
2017-01-23 06:14:15 +0900

    fix StereoSGBM header mistake

--- test/OpenCvSharp.Tests/face/LBPHFaceRecognizerTest.cs
+++ test/OpenCvSharp.Tests/face/LBPHFaceRecognizerTest.cs
@@ -8,10 +8,34 @@ namespace OpenCvSharp.Tests.Face
     public class LBPHFaceRecognizerTest : TestBase
     {
         [Test]
-        public void New()
+        public void CreateAndDispose()
         {
             var recognizer = OpenCvSharp.Face.FaceRecognizer.CreateLBPHFaceRecognizer(1, 8, 8, 8, 123);
             recognizer.Dispose();
+        }
+
+        [Test, Ignore("not implemented")]
+        public void TrainAndPredict()
+        {
+            var image = new Mat("Data/Image/Lenna.png");
+
+            var model = Cv2.CreateLBPHFaceRecognizer();
+
+            var cascade = new CascadeClassifier("../haarcascade_frontalface_default.xml");
+            var rects = cascade.DetectMultiScale(image);
+
+            foreach (Rect rect in rects)
+            {
+                using (Mat face = image[rect].Clone())
+                {
+                    Cv2.Resize(face, face, new Size(256, 256));
+
+                    int label;
+                    double confidence;
+                    model.Predict(face, out label, out confidence);
+                    Console.WriteLine($"{label} ({confidence})");
+                }
+            }
         }
     }
 }
