91137811c5b6a5c694901c0bf7150827f19af577
shimat <schimatk@gmail.com>
2017-01-22 18:20:16 +0900

    add BOWKMeansTrainer test

---
+++ test/OpenCvSharp.Tests/features2d/BOWKMeansTrainer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace OpenCvSharp.Tests.Features2D
+{
+    [TestFixture]
+    public class BOWKMeansTrainerTest
+    {
+        [Test]
+        public void New()
+        {
+            var bow = new BOWKMeansTrainer(100);
+            bow.Dispose();
+        }
+    }
+}
+
