99d56335eccec5ec7e9804c56197ab5247c6a7a9
shimat <schimatk@gmail.com>
2017-02-04 21:50:04 +0900

    add Thinning and NiblackThreshold

---
+++ src/OpenCvSharp/PInvoke/ximgproc/NativeMethods_ximgproc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+// ReSharper disable InconsistentNaming
+
+#pragma warning disable 1591
+
+namespace OpenCvSharp
+{
+    static partial class NativeMethods
+    {
+        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void ximgproc_niBlackThreshold(
+            IntPtr src, IntPtr dst, double maxValue, int type,
+            int blockSize, double delta);
+
+        [DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void ximgproc_thinning(IntPtr src, IntPtr dst, int thinningType);
+    }
+}
