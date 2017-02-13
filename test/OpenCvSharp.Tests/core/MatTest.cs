8c0e6554ca73f5ac3ae6a1add0df5b0e4367b867
shimat <schimatk@gmail.com>
2017-02-13 10:32:23 +0900

    added MatOfXXX.FromArray(T[,]) tests

--- test/OpenCvSharp.Tests/core/MatTest.cs
+++ test/OpenCvSharp.Tests/core/MatTest.cs
@@ -13,9 +13,28 @@ namespace OpenCvSharp.Tests.Core
             var array = new double[] {7, 8, 9};
             var m = MatOfDouble.FromArray(array);
 
+            var indexer = m.GetIndexer();
             for (int i = 0; i < array.Length; i++)
             {
                 Assert.That(m.Get<double>(i), Is.EqualTo(array[i]).Within(1e-6));
+                Assert.That(indexer[i], Is.EqualTo(array[i]).Within(1e-6));
+            }
+        }
+
+        [Test]
+        public void MatOfDoubleFromRectangularArray()
+        {
+            var array = new double[,] {{1, 2}, {3, 4}};
+            var m = MatOfDouble.FromArray(array);
+
+            var indexer = m.GetIndexer();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Assert.That(m.Get<double>(i, j), Is.EqualTo(array[i, j]).Within(1e-6));
+                    Assert.That(indexer[i, j], Is.EqualTo(array[i, j]).Within(1e-6));
+                }
             }
         }
 
@@ -25,9 +44,28 @@ namespace OpenCvSharp.Tests.Core
             var array = new float[] { 7, 8, 9 };
             var m = MatOfFloat.FromArray(array);
 
+            var indexer = m.GetIndexer();
             for (int i = 0; i < array.Length; i++)
             {
                 Assert.That(m.Get<float>(i), Is.EqualTo(array[i]).Within(1e-6));
+                Assert.That(indexer[i], Is.EqualTo(array[i]).Within(1e-6));
+            }
+        }
+
+        [Test]
+        public void MatOfFloatFromRectangularArray()
+        {
+            var array = new float[,] { { 1, 2 }, { 3, 4 } };
+            var m = MatOfFloat.FromArray(array);
+
+            var indexer = m.GetIndexer();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Assert.That(m.Get<float>(i, j), Is.EqualTo(array[i, j]).Within(1e-6));
+                    Assert.That(indexer[i, j], Is.EqualTo(array[i, j]).Within(1e-6));
+                }
             }
         }
 
@@ -37,9 +75,28 @@ namespace OpenCvSharp.Tests.Core
             var array = new int[] { 7, 8, 9 };
             var m = MatOfInt.FromArray(array);
 
+            var indexer = m.GetIndexer();
             for (int i = 0; i < array.Length; i++)
             {
                 Assert.That(m.Get<int>(i), Is.EqualTo(array[i]));
+                Assert.That(indexer[i], Is.EqualTo(array[i]));
+            }
+        }
+
+        [Test]
+        public void MatOfIntFromRectangularArray()
+        {
+            var array = new int[,] { { 1, 2 }, { 3, 4 } };
+            var m = MatOfInt.FromArray(array);
+
+            var indexer = m.GetIndexer();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Assert.That(m.Get<int>(i, j), Is.EqualTo(array[i, j]));
+                    Assert.That(indexer[i, j], Is.EqualTo(array[i, j]));
+                }
             }
         }
 
@@ -49,9 +106,28 @@ namespace OpenCvSharp.Tests.Core
             var array = new ushort[] { 7, 8, 9 };
             var m = MatOfUShort.FromArray(array);
 
+            var indexer = m.GetIndexer();
             for (int i = 0; i < array.Length; i++)
             {
                 Assert.That(m.Get<ushort>(i), Is.EqualTo(array[i]));
+                Assert.That(indexer[i], Is.EqualTo(array[i]));
+            }
+        }
+
+        [Test]
+        public void MatOfUShortFromRectangularArray()
+        {
+            var array = new ushort[,] { { 1, 2 }, { 3, 4 } };
+            var m = MatOfUShort.FromArray(array);
+
+            var indexer = m.GetIndexer();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Assert.That(m.Get<ushort>(i, j), Is.EqualTo(array[i, j]));
+                    Assert.That(indexer[i, j], Is.EqualTo(array[i, j]));
+                }
             }
         }
 
@@ -61,9 +137,28 @@ namespace OpenCvSharp.Tests.Core
             var array = new short[] { 7, 8, 9 };
             var m = MatOfShort.FromArray(array);
 
+            var indexer = m.GetIndexer();
             for (int i = 0; i < array.Length; i++)
             {
                 Assert.That(m.Get<short>(i), Is.EqualTo(array[i]));
+                Assert.That(indexer[i], Is.EqualTo(array[i]));
+            }
+        }
+
+        [Test]
+        public void MatOfShortFromRectangularArray()
+        {
+            var array = new short[,] { { 1, 2 }, { 3, 4 } };
+            var m = MatOfShort.FromArray(array);
+
+            var indexer = m.GetIndexer();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Assert.That(m.Get<short>(i, j), Is.EqualTo(array[i, j]));
+                    Assert.That(indexer[i, j], Is.EqualTo(array[i, j]));
+                }
             }
         }
 
@@ -73,9 +168,28 @@ namespace OpenCvSharp.Tests.Core
             var array = new byte[] { 7, 8, 9 };
             var m = MatOfByte.FromArray(array);
 
+            var indexer = m.GetIndexer();
             for (int i = 0; i < array.Length; i++)
             {
                 Assert.That(m.Get<byte>(i), Is.EqualTo(array[i]));
+                Assert.That(indexer[i], Is.EqualTo(array[i]));
+            }
+        }
+
+        [Test]
+        public void MatOfByteFromRectangularArray()
+        {
+            var array = new byte[,] { { 1, 2 }, { 3, 4 } };
+            var m = MatOfByte.FromArray(array);
+
+            var indexer = m.GetIndexer();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Assert.That(m.Get<byte>(i, j), Is.EqualTo(array[i, j]));
+                    Assert.That(indexer[i, j], Is.EqualTo(array[i, j]));
+                }
             }
         }
     }
