6192fd8feaeec3a7c25b232b9d491676ab317b98
shimat <schimatk@gmail.com>
2017-02-16 16:49:33 +0900

    remove unused C++ files

--- src/OpenCvSharpExtern/gpu.cpp
+++
@@ -1,7 +0,0 @@
-#include "gpu.h"
-#include "gpu_GpuMat.h"
-#include "gpu_CvMethods.h"
-#include "gpu_HOGDescriptor.h"
-#include "gpu_StereoBM_GPU.h"
-#include "gpu_CascadeClassifier_GPU.h"
-#include "gpu_MOG_GPU.h"
--- src/OpenCvSharpExtern/nonfree.cpp
+++
@@ -1,1 +0,0 @@
-#include "nonfree.h"
