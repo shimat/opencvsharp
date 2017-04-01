e1457ce60a2e1a38e550694f508a14711913c176
shimat <schimatk@gmail.com>
2017-04-01 22:36:40 +0900

    added virtual DisposeManaged and DisposeUnmanaged methods

--- src/OpenCvSharp/Fundamentals/DisposableCvObject.cs
+++ src/OpenCvSharp/Fundamentals/DisposableCvObject.cs
@@ -89,41 +89,14 @@ namespace OpenCvSharp
             this.ptr = ptr;
         }
 
-#if LANG_JP
         /// <summary>
-        /// リソースの解放
+        /// releases unmanaged resources
         /// </summary>
-        /// <param name="disposing">
-        /// trueの場合は、このメソッドがユーザコードから直接が呼ばれたことを示す。マネージ・アンマネージ双方のリソースが解放される。
-        /// falseの場合は、このメソッドはランタイムからファイナライザによって呼ばれ、もうほかのオブジェクトから参照されていないことを示す。アンマネージリソースのみ解放される。
-        ///</param>
-#else
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">
-        /// If disposing equals true, the method has been called directly or indirectly by a user's code. Managed and unmanaged resources can be disposed.
-        /// If false, the method has been called by the runtime from inside the finalizer and you should not reference other objects. Only unmanaged resources can be disposed.
-        /// </param>
-#endif
-        protected override void Dispose(bool disposing)
+        protected override void DisposeUnmanaged()
         {
-            if (!disposed)
-            {
-                try
-                {
-                    if (disposing)
-                    {
-                    }
-                    ptr = IntPtr.Zero;
-                    disposed = true;
-                }
-                finally
-                {
-                    base.Dispose(disposing);
-                }
-            }
+            ptr = IntPtr.Zero;
         }
+        
         #endregion
 
 #if LANG_JP
