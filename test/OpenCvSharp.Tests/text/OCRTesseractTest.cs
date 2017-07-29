5beb4c9868c62cd4b432a6d0597ae73a144e8260
shimat <schimatk@gmail.com>
2017-07-29 23:46:42 +0900

    added tesseract binaries

--- test/OpenCvSharp.Tests/text/OCRTesseractTest.cs
+++ test/OpenCvSharp.Tests/text/OCRTesseractTest.cs
@@ -12,19 +12,24 @@ namespace OpenCvSharp.Tests.Text
     [TestFixture]
     public class OCRTesseractTest : TestBase
     {
+        private const string TessData = @"_data\tessdata";
+
         [Test]
         public void Create()
         {
-            using (var tesseract = OCRTesseract.Create())
+            using (var tesseract = OCRTesseract.Create(TessData))
             {
+                GC.KeepAlive(tesseract);
             }
         }
 
         [Test]
         public void Run()
         {
+            Console.WriteLine(Directory.GetCurrentDirectory());
+
             using (var image = Image("alphabet.png"))
-            using (var tesseract = OCRTesseract.Create())
+            using (var tesseract = OCRTesseract.Create(TessData))
             {
                 tesseract.Run(image,
                     out var outputText, out var componentRects, out var componentTexts, out var componentConfidences);
