447c9cbbdff217b21794f3c02709ca966baceca0
shimat <schimatk@gmail.com>
2017-09-12 13:44:25 +0900

    try to disable parallelization

--- test/OpenCvSharp.Tests/TestBase.cs
+++ test/OpenCvSharp.Tests/TestBase.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using Xunit;
 
-[assembly: CollectionBehavior(MaxParallelThreads = 2)]
+[assembly: CollectionBehavior(/*MaxParallelThreads = 2, */DisableTestParallelization = true)]
 
 namespace OpenCvSharp.Tests
 {
