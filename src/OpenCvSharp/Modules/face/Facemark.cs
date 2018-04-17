f0da39929c32458d8a89b41bcbddf934ce2b649c
shimat <schimatk@gmail.com>
2018-04-17 15:08:41 +0900

    add Facemark

---
+++ src/OpenCvSharp/Modules/face/Facemark.cs
@@ -0,0 +1,190 @@
+﻿using System;
+
+namespace OpenCvSharp.Face
+{
+    /// <summary>
+    /// Abstract base class for all facemark models.
+    /// 
+    /// All facemark models in OpenCV are derived from the abstract base class Facemark, which 
+    /// provides a unified access to all facemark algorithms in OpenCV.
+    /// To utilize this API in your program, please take a look at the @ref tutorial_table_of_content_facemark
+    /// </summary>
+    public abstract class Facemark : Algorithm
+    {
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        /// <param name="fn"></param>
+        public override void Read(FileNode fn)
+        {
+            ThrowIfDisposed();
+            if (fn == null)
+                throw new ArgumentNullException(nameof(fn));
+           
+            NativeMethods.face_Facemark_read(ptr, fn.CvPtr);
+
+            GC.KeepAlive(this);
+            GC.KeepAlive(fn);
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        /// <param name="fs"></param>
+        public override void Write(FileStorage fs)
+        {
+            ThrowIfDisposed();
+            if (fs == null)
+                throw new ArgumentNullException(nameof(fs));
+
+            NativeMethods.face_Facemark_write(ptr, fs.CvPtr);
+
+            GC.KeepAlive(this);
+            GC.KeepAlive(fs);
+        }
+
+        /// <summary>
+        /// Add one training sample to the trainer.
+        /// </summary>
+        /// <param name="image">Input image.</param>
+        /// <param name="landmarks">The ground-truth of facial landmarks points corresponds to the image.</param>
+        /// <returns></returns>
+        public virtual bool AddTrainingSample(InputArray image, InputArray landmarks)
+        {
+            ThrowIfDisposed();
+            if (image == null)
+                throw new ArgumentNullException(nameof(image));
+            if (landmarks == null)
+                throw new ArgumentNullException(nameof(landmarks));
+            image.ThrowIfDisposed();
+            landmarks.ThrowIfDisposed();
+
+            int ret = NativeMethods.face_Facemark_addTrainingSample(ptr, image.CvPtr, landmarks.CvPtr);
+
+            GC.KeepAlive(this);
+            GC.KeepAlive(image);
+            GC.KeepAlive(landmarks);
+
+            return ret != 0;
+        }
+
+        /// <summary>
+        /// Trains a Facemark algorithm using the given dataset.
+        /// Before the training process, training samples should be added to the trainer 
+        /// using face::addTrainingSample function.
+        /// </summary>
+        /// <param name="parameters">Optional extra parameters (algorithm dependent).</param>
+        public virtual void Training(IntPtr parameters)
+        {
+            ThrowIfDisposed();
+            NativeMethods.face_Facemark_training(ptr, parameters);
+            GC.KeepAlive(this);
+        }
+
+        /// <summary>
+        ///  A function to load the trained model before the fitting process.
+        /// </summary>
+        /// <param name="model">A string represent the filename of a trained model.</param>
+        public virtual void LoadModel(string model)
+        {
+            ThrowIfDisposed();
+            NativeMethods.face_Facemark_loadModel(ptr, model);
+            GC.KeepAlive(this);
+        }
+
+        /// <summary>
+        /// Trains a Facemark algorithm using the given dataset.
+        /// </summary>
+        /// <param name="image">Input image.</param>
+        /// <param name="faces">Output of the function which represent region of interest of the detected faces. Each face is stored in cv::Rect container.</param>
+        /// <param name="landmarks">The detected landmark points for each faces.</param>
+        /// <returns></returns>
+        public virtual bool Fit(
+            InputArray image,
+            InputArray faces,
+            InputOutputArray landmarks)
+        {
+            return Fit(image, faces, landmarks, IntPtr.Zero);
+        }
+
+        /// <summary>
+        /// Trains a Facemark algorithm using the given dataset.
+        /// </summary>
+        /// <param name="image">Input image.</param>
+        /// <param name="faces">Output of the function which represent region of interest of the detected faces. Each face is stored in cv::Rect container.</param>
+        /// <param name="landmarks">The detected landmark points for each faces.</param>
+        /// <param name="config">Algorithm specific for running time parameters.</param>
+        /// <returns></returns>
+        public virtual bool Fit(
+            InputArray image,
+            InputArray faces,
+            InputOutputArray landmarks,
+            IntPtr config)
+        {
+            ThrowIfDisposed();
+            if (image == null)
+                throw new ArgumentNullException(nameof(image));
+            if (faces == null)
+                throw new ArgumentNullException(nameof(faces));
+            if (landmarks == null)
+                throw new ArgumentNullException(nameof(landmarks));
+            image.ThrowIfDisposed();
+            faces.ThrowIfDisposed();
+            landmarks.ThrowIfNotReady();
+
+            int ret = NativeMethods.face_Facemark_fit(ptr, image.CvPtr, faces.CvPtr, landmarks.CvPtr, config);
+
+            GC.KeepAlive(this);
+            GC.KeepAlive(image);
+            GC.KeepAlive(landmarks);
+
+            return ret != 0;
+        }
+
+        /// <summary>
+        /// Set a user defined face detector for the Facemark algorithm.
+        /// </summary>
+        /// <param name="detector">The user defined face detector function</param>
+        /// <param name="userData">Detector parameters</param>
+        /// <returns></returns>
+        public virtual bool SetFaceDetector(IntPtr detector, IntPtr userData)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Detect faces from a given image using default or user defined face detector.
+        /// Some Algorithm might not provide a default face detector.
+        /// </summary>
+        /// <param name="image">Input image.</param>
+        /// <param name="faces">Output of the function which represent region of interest of the detected faces. Each face is stored in cv::Rect container.</param>
+        /// <returns></returns>
+        public virtual bool GetFaces(InputArray image, OutputArray faces)
+        {
+            ThrowIfDisposed();
+            if (image == null)
+                throw new ArgumentNullException(nameof(image));
+            if (faces == null)
+                throw new ArgumentNullException(nameof(faces));
+            image.ThrowIfDisposed();
+            faces.ThrowIfDisposed();
+
+            int ret = NativeMethods.face_Facemark_getFaces(ptr, image.CvPtr, faces.CvPtr);
+
+            GC.KeepAlive(this);
+            GC.KeepAlive(image);
+
+            return ret != 0;
+        }
+
+        /// <summary>
+        /// Get data from an algorithm
+        /// </summary>
+        /// <param name="items"> The obtained data, algorithm dependent.</param>
+        /// <returns></returns>
+        public virtual bool GetData(IntPtr items)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/OpenCvSharpExtern/face_Facemark.h
@@ -0,0 +1,259 @@
+#ifndef _CPP_FACE_FACEMARK_H_
+#define _CPP_FACE_FACEMARK_H_
+
+// ReSharper disable CppNonInlineFunctionDefinitionInHeaderFile
+
+#include "include_opencv.h"
+using namespace cv::face;
+
+
+#pragma region Facemark
+
+CVAPI(void) face_Facemark_read(Facemark *obj, cv::FileNode *fn)
+{
+    obj->read(*fn);
+}
+
+CVAPI(void) face_Facemark_write(Facemark *obj, cv::FileStorage *fs)
+{
+    obj->write(*fs);
+}
+
+CVAPI(int) face_Facemark_addTrainingSample(Facemark *obj, cv::_InputArray *image, cv::_InputArray *landmarks)
+{
+    return obj->addTrainingSample(*image, *landmarks) ? 1 : 0;
+}
+
+CVAPI(void) face_Facemark_training(Facemark *obj, void* parameters)
+{
+    obj->training(parameters);
+}
+
+CVAPI(void) face_Facemark_loadModel(Facemark *obj, const char *model)
+{
+    obj->loadModel(model);
+}
+
+CVAPI(int) face_Facemark_fit(Facemark *obj, 
+    cv::_InputArray *image,
+    cv::_InputArray *faces,
+    cv::_InputOutputArray *landmarks,
+    void * config)
+{
+    return obj->fit(*image, *faces, *landmarks, config) ? 1 : 0;
+}
+
+CVAPI(int) face_Facemark_setFaceDetector(Facemark *obj, FN_FaceDetector detector, void* userData)
+{
+    return obj->setFaceDetector(detector, userData) ? 1 : 0;
+}
+
+CVAPI(int) face_Facemark_getFaces(Facemark *obj, cv::_InputArray *image, cv::_OutputArray *faces)
+{
+    return obj->getFaces(*image, *faces) ? 1 : 0;
+}
+
+#pragma endregion
+
+#pragma region FacemarkLBF
+
+CVAPI(cv::Ptr<FacemarkLBF>*) face_FacemarkLBF_create(FacemarkLBF::Params *params)
+{
+    const auto r = FacemarkLBF::create(*params);
+    return clone(r);
+}
+
+CVAPI(FacemarkLBF*) face_Ptr_FacemarkLBF_get(cv::Ptr<FacemarkLBF> *obj)
+{
+    return obj->get();
+}
+
+CVAPI(void) face_Ptr_FacemarkLBF_delete(cv::Ptr<FacemarkLBF> *obj)
+{
+    delete obj;
+}
+
+#pragma region Params
+
+CVAPI(FacemarkLBF::Params*) face_FacemarkLBF_Params_new()
+{
+    return new FacemarkLBF::Params;
+}
+
+CVAPI(void) face_FacemarkLBF_Params_delete(FacemarkLBF::Params *obj)
+{
+    delete obj;
+}
+
+CVAPI(double) face_FacemarkLBF_Params_shape_offset_get(FacemarkLBF::Params *obj)
+{
+    return obj->shape_offset;
+}
+CVAPI(void) face_FacemarkLBF_Params_shape_offset_set(FacemarkLBF::Params *obj, double val)
+{
+    obj->shape_offset = val;
+}
+
+CVAPI(void) face_FacemarkLBF_Params_cascade_face_get(FacemarkLBF::Params *obj, std::string *s)
+{
+    *s = obj->cascade_face;
+}
+CVAPI(void) face_FacemarkLBF_Params_cascade_face_set(FacemarkLBF::Params *obj, const char *s)
+{
+     obj->cascade_face = s;
+}
+
+CVAPI(int) face_FacemarkLBF_Params_verbose_get(FacemarkLBF::Params *obj)
+{
+    return obj->verbose ? 1 : 0;
+}
+CVAPI(void) face_FacemarkLBF_Params_verbose_set(FacemarkLBF::Params *obj, int val)
+{
+    obj->shape_offset = (val != 0);
+}
+
+CVAPI(int) face_FacemarkLBF_Params_n_landmarks_get(FacemarkLBF::Params *obj)
+{
+    return obj->n_landmarks;
+}
+CVAPI(void) face_FacemarkLBF_Params_n_landmarks_set(FacemarkLBF::Params *obj, int val)
+{
+    obj->n_landmarks = val;
+}
+
+CVAPI(int) face_FacemarkLBF_Params_initShape_n_get(FacemarkLBF::Params *obj)
+{
+    return obj->initShape_n;
+}
+CVAPI(void) face_FacemarkLBF_Params_initShape_n_set(FacemarkLBF::Params *obj, int val)
+{
+    obj->initShape_n = val;
+}
+
+CVAPI(int) face_FacemarkLBF_Params_stages_n_get(FacemarkLBF::Params *obj)
+{
+    return obj->stages_n;
+}
+CVAPI(void) face_FacemarkLBF_Params_stages_n_set(FacemarkLBF::Params *obj, int val)
+{
+    obj->stages_n = val;
+}
+
+CVAPI(int) face_FacemarkLBF_Params_tree_n_get(FacemarkLBF::Params *obj)
+{
+    return obj->tree_n;
+}
+CVAPI(void) face_FacemarkLBF_Params_tree_n_set(FacemarkLBF::Params *obj, int val)
+{
+    obj->tree_n = val;
+}
+
+CVAPI(int) face_FacemarkLBF_Params_tree_depth_get(FacemarkLBF::Params *obj)
+{
+    return obj->tree_depth;
+}
+CVAPI(void) face_FacemarkLBF_Params_tree_depth_set(FacemarkLBF::Params *obj, int val)
+{
+    obj->tree_depth = val;
+}
+
+CVAPI(double) face_FacemarkLBF_Params_bagging_overlap_get(FacemarkLBF::Params *obj)
+{
+    return obj->bagging_overlap;
+}
+CVAPI(void) face_FacemarkLBF_Params_bagging_overlap_set(FacemarkLBF::Params *obj, double val)
+{
+    obj->bagging_overlap = val;
+}
+
+CVAPI(void) face_FacemarkLBF_Params_model_filename_get(FacemarkLBF::Params *obj, std::string *s)
+{
+    *s = obj->model_filename;
+}
+CVAPI(void) face_FacemarkLBF_Params_model_filename_set(FacemarkLBF::Params *obj, const char *s)
+{
+    obj->model_filename = s;
+}
+
+CVAPI(int) face_FacemarkLBF_Params_save_model_get(FacemarkLBF::Params *obj)
+{
+    return obj->save_model ? 1 : 0;
+}
+CVAPI(void) face_FacemarkLBF_Params_save_model_set(FacemarkLBF::Params *obj, int val)
+{
+    obj->save_model = (val != 0);
+}
+
+CVAPI(unsigned int) face_FacemarkLBF_Params_seed_get(FacemarkLBF::Params *obj)
+{
+    return obj->seed;
+}
+CVAPI(void) face_FacemarkLBF_Params_seed_set(FacemarkLBF::Params *obj, unsigned int val)
+{
+    obj->seed = val;
+}
+
+CVAPI(void) face_FacemarkLBF_Params_feats_m_get(FacemarkLBF::Params *obj, std::vector<int> *v)
+{
+    std::copy(obj->feats_m.begin(), obj->feats_m.end(), std::back_inserter(*v));
+}
+CVAPI(void) face_FacemarkLBF_Params_feats_m_set(FacemarkLBF::Params *obj, std::vector<int> *v)
+{
+    obj->feats_m.clear();
+    std::copy(v->begin(), v->end(), std::back_inserter(obj->feats_m));
+}
+
+CVAPI(void) face_FacemarkLBF_Params_radius_m_get(FacemarkLBF::Params *obj, std::vector<double> *v)
+{
+    std::copy(obj->radius_m.begin(), obj->radius_m.end(), std::back_inserter(*v));
+}
+CVAPI(void) face_FacemarkLBF_Params_radius_m_set(FacemarkLBF::Params *obj, std::vector<double> *v)
+{
+    obj->radius_m.clear();
+    std::copy(v->begin(), v->end(), std::back_inserter(obj->radius_m));
+}
+
+CVAPI(void) face_FacemarkLBF_Params_pupils0_get(FacemarkLBF::Params *obj, std::vector<int> *v)
+{
+    std::copy(obj->pupils[0].begin(), obj->pupils[0].end(), std::back_inserter(*v));
+}
+CVAPI(void) face_FacemarkLBF_Params_pupils0_set(FacemarkLBF::Params *obj, std::vector<int> *v)
+{
+    obj->pupils[0].clear();
+    std::copy(v->begin(), v->end(), std::back_inserter(obj->pupils[0]));
+}
+
+CVAPI(void) face_FacemarkLBF_Params_pupils1_get(FacemarkLBF::Params *obj, std::vector<int> *v)
+{
+    std::copy(obj->pupils[1].begin(), obj->pupils[1].end(), std::back_inserter(*v));
+}
+CVAPI(void) face_FacemarkLBF_Params_pupils1_set(FacemarkLBF::Params *obj, std::vector<int> *v)
+{
+    obj->pupils[0].clear();
+    std::copy(v->begin(), v->end(), std::back_inserter(obj->pupils[1]));
+}
+
+CVAPI(MyCvRect) face_FacemarkLBF_Params_detectROI_get(FacemarkLBF::Params *obj)
+{
+    return c(obj->detectROI);
+}
+CVAPI(void) face_FacemarkLBF_Params_detectROI_set(FacemarkLBF::Params *obj, MyCvRect val)
+{
+    obj->detectROI = cpp(val);
+}
+
+
+CVAPI(void) face_FacemarkLBF_Params_read(FacemarkLBF::Params *obj, cv::FileNode *fn)
+{
+    obj->read(*fn);
+}
+
+CVAPI(void) face_FacemarkLBF_Params_write(FacemarkLBF::Params *obj, cv::FileStorage *fs)
+{
+    obj->write(*fs);
+}
+
+#pragma endregion
+#pragma endregion
+
+#endif
