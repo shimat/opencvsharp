2fa3c1299090b6331a0b219a68f97b677e823c6f
shimat <schimatk@gmail.com>
2018-05-10 18:01:44 +0900

    add managed Partition()

--- test/OpenCvSharp.Tests/core/CoreTest.cs
+++ test/OpenCvSharp.Tests/core/CoreTest.cs
@@ -130,6 +130,17 @@ namespace OpenCvSharp.Tests.Core
                 Assert.Equal(0, z.Get<double>(2));
             }
         }
+
+        [Fact]
+        public void Partition()
+        {
+            var array = new[] {1, 3, 8, 8, 1, 3, 3, };
+
+            int nClasses = Cv2.Partition(array, out var labels, (a, b) => a == b);
+
+            Assert.Equal(3, nClasses);
+            Assert.Equal(new[] {0, 1, 2, 2, 0, 1, 1}, labels);
+        }
     }
 }
 
