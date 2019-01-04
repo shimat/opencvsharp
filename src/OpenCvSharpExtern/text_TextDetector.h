c95540ebedcdc83494811b625b038e0ae522aaac
shimat <schimatk@gmail.com>
2019-01-04 17:48:25 +0900

    text/TextDetector (incomplete)

---
+++ src/OpenCvSharpExtern/text_TextDetector.h
@@ -0,0 +1,48 @@
+#ifndef _CPP_TEXT_TEXTDETECTOR_H_
+#define _CPP_TEXT_TEXTDETECTOR_H_
+
+#include "include_opencv.h"
+
+CVAPI(void) text_TextDetector_detect(cv::text::TextDetector *obj, cv::_InputArray *inputImage, std::vector<cv::Rect> *Bbox, std::vector<float> *confidence)
+{
+    obj->detect(*inputImage, *Bbox, *confidence);
+}
+
+CVAPI(void) text_TextDetectorCNN_detect(cv::text::TextDetectorCNN *obj, cv::_InputArray *inputImage, std::vector<cv::Rect> *Bbox, std::vector<float> *confidence)
+{
+    obj->detect(*inputImage, *Bbox, *confidence);
+}
+
+CVAPI(cv::Ptr<cv::text::TextDetectorCNN>*) text_TextDetectorCNN_create1(
+    const char *modelArchFilename, const char *modelWeightsFilename, MyCvSize *detectionSizes, int detectionSizesLength)
+{
+    std::vector<cv::Size> detectionSizesVec;
+    if (detectionSizes != nullptr)
+    {
+        detectionSizesVec.resize(detectionSizesLength);
+        for (size_t i = 0; i < detectionSizesLength; i++)
+            detectionSizesVec[i] = cpp(detectionSizes[i]);
+    }
+
+    const auto ptr = cv::text::TextDetectorCNN::create(modelArchFilename, modelWeightsFilename, detectionSizesVec);
+    return new cv::Ptr<cv::text::TextDetectorCNN>(ptr);
+}
+
+CVAPI(cv::Ptr<cv::text::TextDetectorCNN>*) text_TextDetectorCNN_create2(
+    const char *modelArchFilename, const char *modelWeightsFilename)
+{
+    const auto ptr = cv::text::TextDetectorCNN::create(modelArchFilename, modelWeightsFilename);
+    return new cv::Ptr<cv::text::TextDetectorCNN>(ptr);
+}
+
+CVAPI(void) text_Ptr_TextDetectorCNN_delete(cv::Ptr<cv::text::TextDetectorCNN> *obj)
+{
+    delete obj;
+}
+
+CVAPI(cv::text::TextDetectorCNN*) text_Ptr_TextDetectorCNN_get(cv::Ptr<cv::text::TextDetectorCNN>* obj)
+{
+    return obj->get();
+}
+
+#endif
---
+++ test/OpenCvSharp.Tests/text/TextDetectorTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using OpenCvSharp.Text;
+using Xunit;
+
+namespace OpenCvSharp.Tests.Text
+{
+    public class TextDetectorTest : TestBase
+    {
+        const string modelArch = "_data/text/textbox.prototxt";
+        const string moddelWeights = "TextBoxes_icdar13.caffemodel";
+        const string moddelWeightsUrl = "https://www.dropbox.com/s/g8pjzv2de9gty8g/TextBoxes_icdar13.caffemodel?dl=0";
+
+        public TextDetectorTest()
+        {
+            
+        }
+
+        [Fact]
+        public void Create()
+        {
+            using (var detector = TextDetectorCNN.Create(modelArch, ""))
+            {
+                GC.KeepAlive(            using (var detector = TextDetectorCNN.Create(modelArch, ""))
+                    );
+            }
+        }
+    }
+}
