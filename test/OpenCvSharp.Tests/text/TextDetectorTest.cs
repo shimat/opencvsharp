5c5ef6e66ff3ac109419d62d0208f405da9c52d7
shimat <schimatk@gmail.com>
2019-06-20 13:48:05 +0900

    fix TextDetectorTest

--- test/OpenCvSharp.Tests/text/TextDetectorTest.cs
+++ test/OpenCvSharp.Tests/text/TextDetectorTest.cs
@@ -1,25 +1,28 @@
 ﻿using System;
-using System.Diagnostics;
 using System.IO;
-using System.Net;
-using OpenCvSharp.Text;
+using System.Net.Http;
 using Xunit;
 
 namespace OpenCvSharp.Tests.Text
 {
     public class TextDetectorTest : TestBase
     {
-        const string modelArch = "_data/text/textbox.prototxt";
-        const string modelWeights = "TextBoxes_icdar13.caffemodel";
-        const string modelWeightsUrl = "https://www.dropbox.com/s/g8pjzv2de9gty8g/TextBoxes_icdar13.caffemodel?dl=0";
-
+        private const string modelArch = "_data/text/textbox.prototxt";
+        private const string modelWeights = "_data/model/TextBoxes_icdar13.caffemodel";
+        private static readonly string modelWeightsUrl = string.Format("https://drive.google.com/uc?id={0}&export=download", "10rqbOxZphuwk0TaWCaixIhheIBnxoaxv"); 
+        
         public TextDetectorTest()
-        {
+        { 
             if (!File.Exists(modelWeights))
             {
-                using (var client = new WebClient())
+                var handler = new HttpClientHandler
                 {
-                    client.DownloadFile(modelWeightsUrl, modelWeights);
+                    ServerCertificateCustomValidationCallback = delegate{ return true; }
+                };
+                using (var client = new HttpClient(handler))
+                {
+                    var data = client.GetByteArrayAsync(modelWeightsUrl).GetAwaiter().GetResult();
+                    File.WriteAllBytes(modelWeights, data);
                 }
             }
         }
@@ -29,6 +32,7 @@ namespace OpenCvSharp.Tests.Text
         {
             Assert.True(File.Exists(modelArch), $"modelArch '{modelArch}' not found");
             Assert.True(File.Exists(modelWeights), $"modelWeights '{modelWeights}' not found");
+            Assert.True(new FileInfo(modelWeights).Length > 10_000_000);
 
             using (var detector = TextDetectorCNN.Create(modelArch, modelWeights))
             {
