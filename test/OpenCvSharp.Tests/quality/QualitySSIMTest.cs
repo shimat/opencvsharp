b25ab0acb599db8357b8c1b4936385521c3ca131
shimat <schimatk@gmail.com>
2019-09-08 17:52:16 +0900

    low precision matching

--- test/OpenCvSharp.Tests/quality/QualitySSIMTest.cs
+++ test/OpenCvSharp.Tests/quality/QualitySSIMTest.cs
@@ -15,9 +15,9 @@ namespace OpenCvSharp.Tests.Quality
                 Cv2.GaussianBlur(refImage, targetImage, new Size(5, 5), 15);
 
                 var value = psnr.Compute(targetImage);
-                Assert.Equal(0.718649, value[0], 6);
-                Assert.Equal(0.791417, value[1], 6);
-                Assert.Equal(0.861893, value[2], 6);
+                Assert.Equal(0.718649, value[0], 3);
+                Assert.Equal(0.791417, value[1], 3);
+                Assert.Equal(0.861893, value[2], 3);
             }
         }
 
@@ -30,9 +30,9 @@ namespace OpenCvSharp.Tests.Quality
                 Cv2.GaussianBlur(refImage, targetImage, new Size(5, 5), 15);
 
                 var value = QualitySSIM.Compute(refImage, targetImage, null);
-                Assert.Equal(0.718649, value[0], 6);
-                Assert.Equal(0.791417, value[1], 6);
-                Assert.Equal(0.861893, value[2], 6);
+                Assert.Equal(0.718649, value[0], 3);
+                Assert.Equal(0.791417, value[1], 3);
+                Assert.Equal(0.861893, value[2], 3);
             }
         }
     }
