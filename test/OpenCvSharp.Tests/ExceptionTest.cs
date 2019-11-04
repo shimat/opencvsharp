e624a8964af3cc26f7a9536126dd932a3bc0fda6
shimat <schimatk@gmail.com>
2019-11-04 11:52:05 +0900

    add ExceptionTest

---
+++ test/OpenCvSharp.Tests/ExceptionTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xunit;
+
+namespace OpenCvSharp.Tests
+{
+    // ReSharper disable InconsistentNaming
+    
+    public class ExceptionTest : TestBase
+    {
+        [Fact]
+        public void RedirectError()
+        {
+            using var img = new Mat(3, 3, MatType.CV_8UC1);
+
+            var ex = Assert.Throws<OpenCVException>(() =>
+            {
+                Cv2.GaussianBlur(img, img, new Size(2, 2), 1); 
+            });
+
+            Assert.StartsWith("ksize.width > 0", ex.ErrMsg);
+            Assert.NotEmpty(ex.FileName);
+            Assert.NotEmpty(ex.FuncName);
+            Assert.True(ex.Line > 0);
+            Assert.Equal(ErrorCode.StsAssert, ex.Status);
+        }
+    }
+}
