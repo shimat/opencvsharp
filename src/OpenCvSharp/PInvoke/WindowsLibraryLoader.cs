140cc659bdb1db9b33bbd3eaae0b9cba62c1b410
shimat <schimatk@gmail.com>
2019-11-09 09:11:57 +0900

    modify managed code for uwp support

--- src/OpenCvSharp/PInvoke/WindowsLibraryLoader.cs
+++ src/OpenCvSharp/PInvoke/WindowsLibraryLoader.cs
@@ -249,8 +249,25 @@ namespace OpenCvSharp
             var libraryHandle = IntPtr.Zero;
             var fileName = FixUpDllFileName(Path.Combine(baseDirectory, dllName));
 
+#if WINRT
+            // MP! Note: This is a hack, needs refinement.  We don't need to carry payload of both binaries for WinRT because the appx is platform specific.
+            ProcessArchitectureInfo processInfo = GetProcessArchitecture();
+
+            string cpu = "x86";
+
+            if (processInfo.Architecture == "AMD64")
+                cpu = "x64";
+
+            string dllpath = baseDirectory.Replace($"dll\\{cpu}", "");
+
+            if (cpu == "x64")
+                fileName = $"{dllpath}{dllName}.dll";
+            else
+                fileName = $"{dllpath}{dllName}.dll";
+
             // Show where we're trying to load the file from
             Debug.WriteLine($"Trying to load native library \"{fileName}\"...");
+#endif
 
             if (File.Exists(fileName))
             {
