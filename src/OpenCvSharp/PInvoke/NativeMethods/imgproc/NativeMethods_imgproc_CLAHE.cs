a30f788bad9d0b5782a8e5b5ced7c1f69586c5ee
shimat <schimatk@gmail.com>
2019-12-05 22:57:03 +0900

    CLAHE

--- src/OpenCvSharp/PInvoke/NativeMethods/imgproc/NativeMethods_imgproc_CLAHE.cs
+++ src/OpenCvSharp/PInvoke/NativeMethods/imgproc/NativeMethods_imgproc_CLAHE.cs
@@ -11,31 +11,31 @@ namespace OpenCvSharp
     static partial class NativeMethods
     {
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern IntPtr imgproc_createCLAHE(double clipLimit, Size tileGridSize);
+        public static extern ExceptionStatus imgproc_createCLAHE(double clipLimit, Size tileGridSize, out IntPtr returnValue);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_Ptr_CLAHE_delete(IntPtr obj);
+        public static extern ExceptionStatus imgproc_Ptr_CLAHE_delete(IntPtr obj);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern IntPtr imgproc_Ptr_CLAHE_get(IntPtr obj);
+        public static extern ExceptionStatus imgproc_Ptr_CLAHE_get(IntPtr obj, out IntPtr returnValue);
 
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_CLAHE_apply(IntPtr obj, IntPtr src, IntPtr dst);
+        public static extern ExceptionStatus imgproc_CLAHE_apply(IntPtr obj, IntPtr src, IntPtr dst);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_CLAHE_setClipLimit(IntPtr obj, double clipLimit);
+        public static extern ExceptionStatus imgproc_CLAHE_setClipLimit(IntPtr obj, double clipLimit);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern double imgproc_CLAHE_getClipLimit(IntPtr obj);
+        public static extern ExceptionStatus imgproc_CLAHE_getClipLimit(IntPtr obj, out double returnValue);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_CLAHE_setTilesGridSize(IntPtr obj, Size tileGridSize);
+        public static extern ExceptionStatus imgproc_CLAHE_setTilesGridSize(IntPtr obj, Size tileGridSize);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern Size imgproc_CLAHE_getTilesGridSize(IntPtr obj);
+        public static extern ExceptionStatus imgproc_CLAHE_getTilesGridSize(IntPtr obj, out Size returnValue);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_CLAHE_collectGarbage(IntPtr obj);
+        public static extern ExceptionStatus imgproc_CLAHE_collectGarbage(IntPtr obj);
     }
 }
