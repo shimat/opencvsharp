7ac4d7295264fd20db3f2f38f7024e9de0f57c46
shimat <schimatk@gmail.com>
2019-12-08 09:27:02 +0900

    LDA, SVD

--- src/OpenCvSharp/PInvoke/NativeMethods/core/NativeMethods_core_Classes.cs
+++ src/OpenCvSharp/PInvoke/NativeMethods/core/NativeMethods_core_Classes.cs
@@ -57,77 +57,80 @@ namespace OpenCvSharp
 
         #region SVD
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern IntPtr core_SVD_new1();
+        public static extern ExceptionStatus core_SVD_new1(out IntPtr returnValue);
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern IntPtr core_SVD_new2(IntPtr src, int flags);
-        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void core_SVD_delete(IntPtr obj);
+        public static extern ExceptionStatus core_SVD_new2(IntPtr src, int flags, out IntPtr returnValue);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void core_SVD_operatorThis(IntPtr obj, IntPtr src, int flags);
-        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void core_SVD_backSubst(IntPtr obj, IntPtr rhs, IntPtr dst);
+        public static extern ExceptionStatus core_SVD_delete(IntPtr obj);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void core_SVD_static_compute1(IntPtr src, IntPtr w, IntPtr u, IntPtr vt, int flags);
+        public static extern ExceptionStatus core_SVD_operatorThis(IntPtr obj, IntPtr src, int flags);
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void core_SVD_static_compute2(IntPtr src, IntPtr w, int flags);
-        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void core_SVD_static_backSubst(IntPtr w, IntPtr u, IntPtr vt, IntPtr rhs, IntPtr dst);
-        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void core_SVD_static_solveZ(IntPtr src, IntPtr dst);
+        public static extern ExceptionStatus core_SVD_backSubst(IntPtr obj, IntPtr rhs, IntPtr dst);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern IntPtr core_SVD_u(IntPtr obj);
+        public static extern ExceptionStatus core_SVD_static_compute1(IntPtr src, IntPtr w, IntPtr u, IntPtr vt, int flags);
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern IntPtr core_SVD_w(IntPtr obj);
+        public static extern ExceptionStatus core_SVD_static_compute2(IntPtr src, IntPtr w, int flags);
+
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern IntPtr core_SVD_vt(IntPtr obj);
+        public static extern ExceptionStatus core_SVD_static_backSubst(IntPtr w, IntPtr u, IntPtr vt, IntPtr rhs, IntPtr dst);
+
+        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        public static extern ExceptionStatus core_SVD_static_solveZ(IntPtr src, IntPtr dst);
+
+        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        public static extern ExceptionStatus core_SVD_u(IntPtr obj, out IntPtr returnValue);
+        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        public static extern ExceptionStatus core_SVD_w(IntPtr obj, out IntPtr returnValue);
+        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        public static extern ExceptionStatus core_SVD_vt(IntPtr obj, out IntPtr returnValue);
         #endregion
 
         #region LDA
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern IntPtr core_LDA_new1(int numComponents);
+        public static extern ExceptionStatus core_LDA_new1(int numComponents, out IntPtr returnValue);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern IntPtr core_LDA_new2(IntPtr src, IntPtr labels, int numComponents);
+        public static extern ExceptionStatus core_LDA_new2(IntPtr src, IntPtr labels, int numComponents, out IntPtr returnValue);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void core_LDA_delete(IntPtr obj);
+        public static extern ExceptionStatus core_LDA_delete(IntPtr obj);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void core_LDA_save_String(IntPtr obj, [MarshalAs(UnmanagedType.LPStr)] string filename);
+        public static extern ExceptionStatus core_LDA_save_String(IntPtr obj, [MarshalAs(UnmanagedType.LPStr)] string filename);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void core_LDA_load_String(IntPtr obj, [MarshalAs(UnmanagedType.LPStr)] string filename);
+        public static extern ExceptionStatus core_LDA_load_String(IntPtr obj, [MarshalAs(UnmanagedType.LPStr)] string filename);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void core_LDA_save_FileStorage(IntPtr obj, IntPtr fs);
+        public static extern ExceptionStatus core_LDA_save_FileStorage(IntPtr obj, IntPtr fs);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void core_LDA_load_FileStorage(IntPtr obj, IntPtr node);
+        public static extern ExceptionStatus core_LDA_load_FileStorage(IntPtr obj, IntPtr node);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void core_LDA_compute(IntPtr obj, IntPtr src, IntPtr labels);
+        public static extern ExceptionStatus core_LDA_compute(IntPtr obj, IntPtr src, IntPtr labels);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern IntPtr core_LDA_project(IntPtr obj, IntPtr src);
+        public static extern ExceptionStatus core_LDA_project(IntPtr obj, IntPtr src, out IntPtr returnValue);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern IntPtr core_LDA_reconstruct(IntPtr obj, IntPtr src);
+        public static extern ExceptionStatus core_LDA_reconstruct(IntPtr obj, IntPtr src, out IntPtr returnValue);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern IntPtr core_LDA_eigenvectors(IntPtr obj);
+        public static extern ExceptionStatus core_LDA_eigenvectors(IntPtr obj, out IntPtr returnValue);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern IntPtr core_LDA_eigenvalues(IntPtr obj);
+        public static extern ExceptionStatus core_LDA_eigenvalues(IntPtr obj, out IntPtr returnValue);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern IntPtr core_LDA_subspaceProject(IntPtr w, IntPtr mean, IntPtr src);
+        public static extern ExceptionStatus core_LDA_subspaceProject(IntPtr w, IntPtr mean, IntPtr src, out IntPtr returnValue);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern IntPtr core_LDA_subspaceReconstruct(IntPtr w, IntPtr mean, IntPtr src);
+        public static extern ExceptionStatus core_LDA_subspaceReconstruct(IntPtr w, IntPtr mean, IntPtr src, out IntPtr returnValue);
 
         #endregion
     }
--- src/OpenCvSharpExtern/core_SVD.h
+++ src/OpenCvSharpExtern/core_SVD.h
@@ -1,65 +1,92 @@
 #ifndef _CPP_CORE_SVD_H_
 #define _CPP_CORE_SVD_H_
 
+// ReSharper disable CppInconsistentNaming
+// ReSharper disable CppNonInlineFunctionDefinitionInHeaderFile
+
 #include "include_opencv.h"
 
-CVAPI(cv::SVD*) core_SVD_new1()
+CVAPI(ExceptionStatus) core_SVD_new1(cv::SVD **returnValue)
 {
-    return new cv::SVD;
+    BEGIN_WRAP
+    *returnValue = new cv::SVD;
+    END_WRAP
 }
-CVAPI(cv::SVD*) core_SVD_new2(cv::_InputArray *src, int flags)
+CVAPI(ExceptionStatus) core_SVD_new2(cv::_InputArray *src, int flags, cv::SVD **returnValue)
 {
-    return new cv::SVD(*src, flags);
+    BEGIN_WRAP
+    *returnValue = new cv::SVD(*src, flags);
+    END_WRAP
 }
-CVAPI(void) core_SVD_delete(cv::SVD *obj)
+CVAPI(ExceptionStatus) core_SVD_delete(cv::SVD *obj)
 {
+    BEGIN_WRAP
     delete obj;
+    END_WRAP
 }
 
-CVAPI(void) core_SVD_operatorThis(cv::SVD *obj, cv::_InputArray *src, int flags)
+CVAPI(ExceptionStatus) core_SVD_operatorThis(cv::SVD *obj, cv::_InputArray *src, int flags)
 {
+    BEGIN_WRAP
     (*obj)(*src, flags);
+    END_WRAP
 }
 //! performs back substitution, so that dst is the solution or pseudo-solution of m*dst = rhs, where m is the decomposed matrix
-CVAPI(void) core_SVD_backSubst(cv::SVD *obj, cv::_InputArray *rhs, cv::_OutputArray *dst)
+CVAPI(ExceptionStatus) core_SVD_backSubst(cv::SVD *obj, cv::_InputArray *rhs, cv::_OutputArray *dst)
 {
+    BEGIN_WRAP
     obj->backSubst(*rhs, *dst);
+    END_WRAP
 }
 
 //! decomposes matrix and stores the results to user-provided matrices
-CVAPI(void) core_SVD_static_compute1(cv::_InputArray *src, cv::_OutputArray *w,
-    cv::_OutputArray *u, cv::_OutputArray *vt, int flags)
+CVAPI(ExceptionStatus) core_SVD_static_compute1(cv::_InputArray *src, cv::_OutputArray *w,
+                                                cv::_OutputArray *u, cv::_OutputArray *vt, int flags)
 {
+    BEGIN_WRAP
     cv::SVD::compute(*src, *w, *u, *vt, flags);
+    END_WRAP
 }
 //! computes singular values of a matrix
-CVAPI(void) core_SVD_static_compute2(cv::_InputArray *src, cv::_OutputArray *w, int flags)
+CVAPI(ExceptionStatus) core_SVD_static_compute2(cv::_InputArray *src, cv::_OutputArray *w, int flags)
 {
+    BEGIN_WRAP
     cv::SVD::compute(*src, *w, flags);
+    END_WRAP
 }
 //! performs back substitution
-CVAPI(void) core_SVD_static_backSubst(cv::_InputArray *w, cv::_InputArray *u,
-    cv::_InputArray *vt, cv::_InputArray *rhs, cv::_OutputArray *dst)
+CVAPI(ExceptionStatus) core_SVD_static_backSubst(cv::_InputArray *w, cv::_InputArray *u,
+                                                 cv::_InputArray *vt, cv::_InputArray *rhs, cv::_OutputArray *dst)
 {
+    BEGIN_WRAP
     cv::SVD::backSubst(*w, *u, *vt, *rhs, *dst);
+    END_WRAP
 }
 //! finds dst = arg min_{|dst|=1} |m*dst|
-CVAPI(void) core_SVD_static_solveZ(cv::_InputArray *src, cv::_OutputArray *dst)
+CVAPI(ExceptionStatus) core_SVD_static_solveZ(cv::_InputArray *src, cv::_OutputArray *dst)
 {
+    BEGIN_WRAP
     cv::SVD::solveZ(*src, *dst);
+    END_WRAP
 }
 
-CVAPI(cv::Mat*) core_SVD_u(cv::SVD *obj)
+CVAPI(ExceptionStatus) core_SVD_u(cv::SVD *obj, cv::Mat **returnValue)
 {
-    return new cv::Mat(obj->u);
+    BEGIN_WRAP
+    *returnValue = new cv::Mat(obj->u);
+    END_WRAP
 }
-CVAPI(cv::Mat*) core_SVD_w(cv::SVD *obj)
+CVAPI(ExceptionStatus) core_SVD_w(cv::SVD *obj, cv::Mat **returnValue)
 {
-    return new cv::Mat(obj->w);
+    BEGIN_WRAP
+    *returnValue = new cv::Mat(obj->w);
+    END_WRAP
 }
-CVAPI(cv::Mat*) core_SVD_vt(cv::SVD *obj)
+CVAPI(ExceptionStatus) core_SVD_vt(cv::SVD *obj, cv::Mat **returnValue)
 {
-    return new cv::Mat(obj->vt);
+    BEGIN_WRAP
+    *returnValue = new cv::Mat(obj->vt);
+    END_WRAP
 }
 
 #endif
