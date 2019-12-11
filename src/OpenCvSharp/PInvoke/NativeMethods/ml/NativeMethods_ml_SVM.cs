ce0a2648d99d918d270a31933a9e6c198bb210c1
shimat <schimatk@gmail.com>
2019-12-11 11:09:37 +0900

    SVM

--- src/OpenCvSharp/PInvoke/NativeMethods/ml/NativeMethods_ml_SVM.cs
+++ src/OpenCvSharp/PInvoke/NativeMethods/ml/NativeMethods_ml_SVM.cs
@@ -12,80 +12,80 @@ namespace OpenCvSharp
     static partial class NativeMethods
     {
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int ml_SVM_getType(IntPtr obj);
+        public static extern ExceptionStatus ml_SVM_getType(IntPtr obj, out int returnValue);
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void ml_SVM_setType(IntPtr obj, int val);
+        public static extern ExceptionStatus ml_SVM_setType(IntPtr obj, int val);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern double ml_SVM_getGamma(IntPtr obj);
+        public static extern ExceptionStatus ml_SVM_getGamma(IntPtr obj, out double returnValue);
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void ml_SVM_setGamma(IntPtr obj, double val);
+        public static extern ExceptionStatus ml_SVM_setGamma(IntPtr obj, double val);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern double ml_SVM_getCoef0(IntPtr obj);
+        public static extern ExceptionStatus ml_SVM_getCoef0(IntPtr obj, out double returnValue);
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void ml_SVM_setCoef0(IntPtr obj, double val);
+        public static extern ExceptionStatus ml_SVM_setCoef0(IntPtr obj, double val);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern double ml_SVM_getDegree(IntPtr obj);
+        public static extern ExceptionStatus ml_SVM_getDegree(IntPtr obj, out double returnValue);
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void ml_SVM_setDegree(IntPtr obj, double val);
+        public static extern ExceptionStatus ml_SVM_setDegree(IntPtr obj, double val);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern double ml_SVM_getC(IntPtr obj);
+        public static extern ExceptionStatus ml_SVM_getC(IntPtr obj, out double returnValue);
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void ml_SVM_setC(IntPtr obj, double val);
+        public static extern ExceptionStatus ml_SVM_setC(IntPtr obj, double val);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern double ml_SVM_getP(IntPtr obj);
+        public static extern ExceptionStatus ml_SVM_getP(IntPtr obj, out double returnValue);
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void ml_SVM_setP(IntPtr obj, double val);
+        public static extern ExceptionStatus ml_SVM_setP(IntPtr obj, double val);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern double ml_SVM_getNu(IntPtr obj);
+        public static extern ExceptionStatus ml_SVM_getNu(IntPtr obj, out double returnValue);
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void ml_SVM_setNu(IntPtr obj, double val);
+        public static extern ExceptionStatus ml_SVM_setNu(IntPtr obj, double val);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern IntPtr ml_SVM_getClassWeights(IntPtr obj);
+        public static extern ExceptionStatus ml_SVM_getClassWeights(IntPtr obj, out IntPtr returnValue);
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void ml_SVM_setClassWeights(IntPtr obj, IntPtr val);
+        public static extern ExceptionStatus ml_SVM_setClassWeights(IntPtr obj, IntPtr val);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern TermCriteria ml_SVM_getTermCriteria(IntPtr obj);
+        public static extern ExceptionStatus ml_SVM_getTermCriteria(IntPtr obj, out TermCriteria returnValue);
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void ml_SVM_setTermCriteria(IntPtr obj, TermCriteria val);
+        public static extern ExceptionStatus ml_SVM_setTermCriteria(IntPtr obj, TermCriteria val);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int ml_SVM_getKernelType(IntPtr obj);
+        public static extern ExceptionStatus ml_SVM_getKernelType(IntPtr obj, out int returnValue);
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void ml_SVM_setKernel(IntPtr obj, int kernelType);
+        public static extern ExceptionStatus ml_SVM_setKernel(IntPtr obj, int kernelType);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern IntPtr ml_SVM_getSupportVectors(IntPtr obj);
+        public static extern ExceptionStatus ml_SVM_getSupportVectors(IntPtr obj, out IntPtr returnValue);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern double ml_SVM_getDecisionFunction(
-            IntPtr obj, int i, IntPtr alpha, IntPtr svidx);
+        public static extern ExceptionStatus ml_SVM_getDecisionFunction(
+            IntPtr obj, int i, IntPtr alpha, IntPtr svidx, out double returnValue);
 
         // static
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern ParamGrid ml_SVM_getDefaultGrid(int paramId);
+        public static extern ExceptionStatus ml_SVM_getDefaultGrid(int paramId, out ParamGrid returnValue);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern IntPtr ml_SVM_create();
+        public static extern ExceptionStatus ml_SVM_create(out IntPtr returnValue);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void ml_Ptr_SVM_delete(IntPtr obj);
+        public static extern ExceptionStatus ml_Ptr_SVM_delete(IntPtr obj);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern IntPtr ml_Ptr_SVM_get(IntPtr obj);
+        public static extern ExceptionStatus ml_Ptr_SVM_get(IntPtr obj, out IntPtr returnValue);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern IntPtr ml_SVM_load(string filePath);
+        public static extern ExceptionStatus ml_SVM_load(string filePath, out IntPtr returnValue);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern IntPtr ml_SVM_loadFromString(string strModel);
+        public static extern ExceptionStatus ml_SVM_loadFromString(string strModel, out IntPtr returnValue);
     }
 }
--- src/OpenCvSharp/PInvoke/NativeMethods/ml/NativeMethods_ml_StatModel.cs
+++ src/OpenCvSharp/PInvoke/NativeMethods/ml/NativeMethods_ml_StatModel.cs
@@ -11,37 +11,34 @@ namespace OpenCvSharp
     static partial class NativeMethods
     {
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void ml_StatModel_clear(IntPtr obj);
+        public static extern ExceptionStatus ml_StatModel_clear(IntPtr obj);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int ml_StatModel_getVarCount(IntPtr obj);
+        public static extern ExceptionStatus ml_StatModel_getVarCount(IntPtr obj, out int returnValue);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int ml_StatModel_empty(IntPtr obj);
+        public static extern ExceptionStatus ml_StatModel_empty(IntPtr obj, out int returnValue);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int ml_StatModel_isTrained(IntPtr obj);
+        public static extern ExceptionStatus ml_StatModel_isTrained(IntPtr obj, out int returnValue);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int ml_StatModel_isClassifier(IntPtr obj);
+        public static extern ExceptionStatus ml_StatModel_isClassifier(IntPtr obj, out int returnValue);
         
-        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int ml_StatModel_train1(
-            IntPtr obj, IntPtr trainData, int flags);
+        /*[Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        public static extern ExceptionStatus ml_StatModel_train1(
+            IntPtr obj, IntPtr trainData, int flags, out int returnValue);*/
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int ml_StatModel_train2(
-            IntPtr obj, IntPtr samples, int layout, IntPtr responses);
+        public static extern ExceptionStatus ml_StatModel_train2(
+            IntPtr obj, IntPtr samples, int layout, IntPtr responses, out int returnValue);
         
-        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern float ml_StatModel_calcError(
-            IntPtr obj, IntPtr data, int test, IntPtr resp);
+        /*[Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        public static extern ExceptionStatus ml_StatModel_calcError(
+            IntPtr obj, IntPtr data, int test, IntPtr resp, out float returnValue);*/
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern float ml_StatModel_predict(
-            IntPtr obj, IntPtr samples, IntPtr results, int flags);
-
-        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void ml_StatModel_save(IntPtr obj, string filename);
+        public static extern ExceptionStatus ml_StatModel_predict(
+            IntPtr obj, IntPtr samples, IntPtr results, int flags, out float returnValue);
     }
 }
