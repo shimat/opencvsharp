46d0292bd7726b43934d078545762f2449e3bb6e
shimat <schimatk@gmail.com>
2019-12-11 09:49:42 +0900

    GeneralizedHough

--- src/OpenCvSharp/PInvoke/NativeMethods/imgproc/NativeMethods_imgproc_GeneralizedHough.cs
+++ src/OpenCvSharp/PInvoke/NativeMethods/imgproc/NativeMethods_imgproc_GeneralizedHough.cs
@@ -13,176 +13,176 @@ namespace OpenCvSharp
         // GeneralizedHough
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_GeneralizedHough_setTemplate1(
+        public static extern ExceptionStatus imgproc_GeneralizedHough_setTemplate1(
             IntPtr obj, IntPtr templ, Point templCenter);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_GeneralizedHough_setTemplate2(
+        public static extern ExceptionStatus imgproc_GeneralizedHough_setTemplate2(
             IntPtr obj, IntPtr edges, IntPtr dx, IntPtr dy, Point templCenter);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_GeneralizedHough_detect1(
+        public static extern ExceptionStatus imgproc_GeneralizedHough_detect1(
             IntPtr obj, IntPtr image, IntPtr positions, IntPtr votes);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_GeneralizedHough_detect2(
+        public static extern ExceptionStatus imgproc_GeneralizedHough_detect2(
             IntPtr obj, IntPtr edges, IntPtr dx, IntPtr dy, IntPtr positions, IntPtr votes);
 
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_GeneralizedHough_setCannyLowThresh(IntPtr obj, int val);
+        public static extern ExceptionStatus imgproc_GeneralizedHough_setCannyLowThresh(IntPtr obj, int val);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int imgproc_GeneralizedHough_getCannyLowThresh(IntPtr obj);
-
-
-        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_GeneralizedHough_setCannyHighThresh(IntPtr obj, int val);
-
-        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int imgproc_GeneralizedHough_getCannyHighThresh(IntPtr obj);
+        public static extern ExceptionStatus imgproc_GeneralizedHough_getCannyLowThresh(IntPtr obj, out int returnValue);
 
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_GeneralizedHough_setMinDist(IntPtr obj, double val);
+        public static extern ExceptionStatus imgproc_GeneralizedHough_setCannyHighThresh(IntPtr obj, int val);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern double imgproc_GeneralizedHough_getMinDist(IntPtr obj);
-
-
-        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_GeneralizedHough_setDp(IntPtr obj, double val);
-
-        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern double imgproc_GeneralizedHough_getDp(IntPtr obj);
+        public static extern ExceptionStatus imgproc_GeneralizedHough_getCannyHighThresh(IntPtr obj, out int returnValue);
 
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_GeneralizedHough_setMaxBufferSize(IntPtr obj, int val);
+        public static extern ExceptionStatus imgproc_GeneralizedHough_setMinDist(IntPtr obj, double val);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int imgproc_GeneralizedHough_getMaxBufferSize(IntPtr obj);
+        public static extern ExceptionStatus imgproc_GeneralizedHough_getMinDist(IntPtr obj, out double returnValue);
+
+
+        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        public static extern ExceptionStatus imgproc_GeneralizedHough_setDp(IntPtr obj, double val);
+
+        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        public static extern ExceptionStatus imgproc_GeneralizedHough_getDp(IntPtr obj, out double returnValue);
+
+
+        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        public static extern ExceptionStatus imgproc_GeneralizedHough_setMaxBufferSize(IntPtr obj, int val);
+
+        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        public static extern ExceptionStatus imgproc_GeneralizedHough_getMaxBufferSize(IntPtr obj, out int returnValue);
 
 
 
         // GeneralizedHoughBallard
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern IntPtr imgproc_createGeneralizedHoughBallard();
+        public static extern ExceptionStatus imgproc_createGeneralizedHoughBallard(out IntPtr returnValue);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern IntPtr imgproc_Ptr_GeneralizedHoughBallard_get(IntPtr obj);
+        public static extern ExceptionStatus imgproc_Ptr_GeneralizedHoughBallard_get(IntPtr obj, out IntPtr returnValue);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_Ptr_GeneralizedHoughBallard_delete(IntPtr obj);
-
-
-        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_GeneralizedHoughBallard_setLevels(IntPtr obj, int val);
-
-        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int imgproc_GeneralizedHoughBallard_getLevels(IntPtr obj);
+        public static extern ExceptionStatus imgproc_Ptr_GeneralizedHoughBallard_delete(IntPtr obj);
 
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_GeneralizedHoughBallard_setVotesThreshold(IntPtr obj, int val);
+        public static extern ExceptionStatus imgproc_GeneralizedHoughBallard_setLevels(IntPtr obj, int val);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int imgproc_GeneralizedHoughBallard_getVotesThreshold(IntPtr obj);
+        public static extern ExceptionStatus imgproc_GeneralizedHoughBallard_getLevels(IntPtr obj, out int returnValue);
+
+
+        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        public static extern ExceptionStatus imgproc_GeneralizedHoughBallard_setVotesThreshold(IntPtr obj, int val);
+
+        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        public static extern ExceptionStatus imgproc_GeneralizedHoughBallard_getVotesThreshold(IntPtr obj, out int returnValue);
 
 
 
         // GeneralizedHoughGuil
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern IntPtr imgproc_createGeneralizedHoughGuil();
+        public static extern ExceptionStatus imgproc_createGeneralizedHoughGuil(out IntPtr returnValue);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern IntPtr imgproc_Ptr_GeneralizedHoughGuil_get(IntPtr obj);
+        public static extern ExceptionStatus imgproc_Ptr_GeneralizedHoughGuil_get(IntPtr obj, out IntPtr returnValue);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_Ptr_GeneralizedHoughGuil_delete(IntPtr obj);
-
-
-        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_GeneralizedHoughGuil_setXi(IntPtr obj, double val);
-
-        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern double imgproc_GeneralizedHoughGuil_getXi(IntPtr obj);
-
-        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_GeneralizedHoughGuil_setLevels(IntPtr obj, int val);
-
-        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int imgproc_GeneralizedHoughGuil_getLevels(IntPtr obj);
+        public static extern ExceptionStatus imgproc_Ptr_GeneralizedHoughGuil_delete(IntPtr obj);
 
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_GeneralizedHoughGuil_setAngleEpsilon(IntPtr obj, double val);
+        public static extern ExceptionStatus imgproc_GeneralizedHoughGuil_setXi(IntPtr obj, double val);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern double imgproc_GeneralizedHoughGuil_getAngleEpsilon(IntPtr obj);
-
-
-        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_GeneralizedHoughGuil_setMinAngle(IntPtr obj, double val);
+        public static extern ExceptionStatus imgproc_GeneralizedHoughGuil_getXi(IntPtr obj, out double returnValue);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern double imgproc_GeneralizedHoughGuil_getMinAngle(IntPtr obj);
-
-
-        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_GeneralizedHoughGuil_setMaxAngle(IntPtr obj, double val);
+        public static extern ExceptionStatus imgproc_GeneralizedHoughGuil_setLevels(IntPtr obj, int val);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern double imgproc_GeneralizedHoughGuil_getMaxAngle(IntPtr obj);
+        public static extern ExceptionStatus imgproc_GeneralizedHoughGuil_getLevels(IntPtr obj, out int returnValue);
 
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_GeneralizedHoughGuil_setAngleStep(IntPtr obj, double val);
+        public static extern ExceptionStatus imgproc_GeneralizedHoughGuil_setAngleEpsilon(IntPtr obj, double val);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern double imgproc_GeneralizedHoughGuil_getAngleStep(IntPtr obj);
-
-
-        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_GeneralizedHoughGuil_setAngleThresh(IntPtr obj, int val);
-
-        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int imgproc_GeneralizedHoughGuil_getAngleThresh(IntPtr obj);
+        public static extern ExceptionStatus imgproc_GeneralizedHoughGuil_getAngleEpsilon(IntPtr obj, out double returnValue);
 
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_GeneralizedHoughGuil_setMinScale(IntPtr obj, double val);
+        public static extern ExceptionStatus imgproc_GeneralizedHoughGuil_setMinAngle(IntPtr obj, double val);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern double imgproc_GeneralizedHoughGuil_getMinScale(IntPtr obj);
-
-        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_GeneralizedHoughGuil_setMaxScale(IntPtr obj, double val);
-
-        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern double imgproc_GeneralizedHoughGuil_getMaxScale(IntPtr obj);
+        public static extern ExceptionStatus imgproc_GeneralizedHoughGuil_getMinAngle(IntPtr obj, out double returnValue);
 
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_GeneralizedHoughGuil_setScaleStep(IntPtr obj, double val);
+        public static extern ExceptionStatus imgproc_GeneralizedHoughGuil_setMaxAngle(IntPtr obj, double val);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern double imgproc_GeneralizedHoughGuil_getScaleStep(IntPtr obj);
-
-
-        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_GeneralizedHoughGuil_setScaleThresh(IntPtr obj, int val);
-
-        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int imgproc_GeneralizedHoughGuil_getScaleThresh(IntPtr obj);
+        public static extern ExceptionStatus imgproc_GeneralizedHoughGuil_getMaxAngle(IntPtr obj, out double returnValue);
 
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern void imgproc_GeneralizedHoughGuil_setPosThresh(IntPtr obj, int val);
+        public static extern ExceptionStatus imgproc_GeneralizedHoughGuil_setAngleStep(IntPtr obj, double val);
 
         [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int imgproc_GeneralizedHoughGuil_getPosThresh(IntPtr obj);
+        public static extern ExceptionStatus imgproc_GeneralizedHoughGuil_getAngleStep(IntPtr obj, out double returnValue);
+
+
+        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        public static extern ExceptionStatus imgproc_GeneralizedHoughGuil_setAngleThresh(IntPtr obj, int val);
+
+        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        public static extern ExceptionStatus imgproc_GeneralizedHoughGuil_getAngleThresh(IntPtr obj, out int returnValue);
+
+
+        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        public static extern ExceptionStatus imgproc_GeneralizedHoughGuil_setMinScale(IntPtr obj, double val);
+
+        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        public static extern ExceptionStatus imgproc_GeneralizedHoughGuil_getMinScale(IntPtr obj, out double returnValue);
+
+        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        public static extern ExceptionStatus imgproc_GeneralizedHoughGuil_setMaxScale(IntPtr obj, double val);
+
+        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        public static extern ExceptionStatus imgproc_GeneralizedHoughGuil_getMaxScale(IntPtr obj, out double returnValue);
+
+
+        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        public static extern ExceptionStatus imgproc_GeneralizedHoughGuil_setScaleStep(IntPtr obj, double val);
+
+        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        public static extern ExceptionStatus imgproc_GeneralizedHoughGuil_getScaleStep(IntPtr obj, out double returnValue);
+
+
+        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        public static extern ExceptionStatus imgproc_GeneralizedHoughGuil_setScaleThresh(IntPtr obj, int val);
+
+        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        public static extern ExceptionStatus imgproc_GeneralizedHoughGuil_getScaleThresh(IntPtr obj, out int returnValue);
+
+
+        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        public static extern ExceptionStatus imgproc_GeneralizedHoughGuil_setPosThresh(IntPtr obj, int val);
+
+        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        public static extern ExceptionStatus imgproc_GeneralizedHoughGuil_getPosThresh(IntPtr obj, out int returnValue);
     }
 }
