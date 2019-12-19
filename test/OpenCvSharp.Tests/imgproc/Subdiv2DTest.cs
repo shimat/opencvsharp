b1a7582f1008e0fc0ec0438723f6731bbbf22d39
shimat <schimatk@gmail.com>
2019-12-20 06:14:27 +0900

    improc test namespace name

--- test/OpenCvSharp.Tests/imgproc/Subdiv2DTest.cs
+++ test/OpenCvSharp.Tests/imgproc/Subdiv2DTest.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Xunit;
+﻿using Xunit;
 
-namespace OpenCvSharp.Tests.imgproc
+namespace OpenCvSharp.Tests.ImgProc
 {
     public class Subdiv2DTest
     {
