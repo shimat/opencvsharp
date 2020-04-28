b758cae0510632c68bae080e5dfcd9ca25d2a93d
TSoft <26995404+tsoft-lab@users.noreply.github.com>
2020-04-29 03:25:41 +0900

    Add QRCodeDetector P/Invoke function.

--- src/OpenCvSharp/PInvoke/NativeMethods/objdetect/NativeMethods_objdetect_QRCodeDetector.cs
+++ src/OpenCvSharp/PInvoke/NativeMethods/objdetect/NativeMethods_objdetect_QRCodeDetector.cs
@@ -34,5 +34,13 @@ namespace OpenCvSharp
         public static extern ExceptionStatus objdetect_QRCodeDetector_detectAndDecode(
             IntPtr obj, IntPtr img, IntPtr points,
             IntPtr straightQrCode, IntPtr returnValue);
+
+        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, BestFitMapping = false, ThrowOnUnmappableChar = true, ExactSpelling = true)]
+        public static extern ExceptionStatus objdetect_QRCodeDetector_decodeMulti(
+            IntPtr obj, IntPtr img, IntPtr points, IntPtr decodedInfo, out IntPtr straightQrCode, out int returnValue);
+
+        [Pure, DllImport(DllExtern, CallingConvention = CallingConvention.Cdecl, BestFitMapping = false, ThrowOnUnmappableChar = true, ExactSpelling = true)]
+        public static extern ExceptionStatus objdetect_QRCodeDetector_decodeMulti_NoStraightQrCode(
+            IntPtr obj, IntPtr img, IntPtr points, IntPtr decodedInfo, out int returnValue);
     }
 }
