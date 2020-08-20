8e7a7cf7e4d47fe20a15400ec76ec10118a69418
shimat <shima@sansan.com>
2020-08-20 22:51:10 +0900

    LineIteratorTest

--- test/OpenCvSharp.Tests/imgcodecs/ImgCodecsTest.cs
+++ test/OpenCvSharp.Tests/imgcodecs/ImgCodecsTest.cs
@@ -415,7 +415,7 @@ namespace OpenCvSharp.Tests.ImgCodecs
             ShowImagesWhenDebugMode(mat);
         }
 
-        [Fact(Skip = "_")]
+        [Fact]
         public void ImDecodeSpan()
         {
             var imageBytes = File.ReadAllBytes("_data/image/mandrill.png");
