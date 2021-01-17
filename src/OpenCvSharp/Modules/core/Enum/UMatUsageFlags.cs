0c4928556ac4457b8a06b96059eb25a812c4b724
shimat <shima@sansan.com>
2021-01-17 09:15:10 +0900

    fix UMat warnings

--- src/OpenCvSharp/Modules/core/Enum/UMatUsageFlags.cs
+++ src/OpenCvSharp/Modules/core/Enum/UMatUsageFlags.cs
@@ -2,25 +2,18 @@
 
 namespace OpenCvSharp
 {
-#if LANG_JP
     /// <summary>
     /// cv::UMatUsageFlags
     /// </summary>
-#else
-    /// <summary>
-    /// Usage flags for allocator.
-    /// </summary>
-#endif
     [Flags]
     public enum UMatUsageFlags
     {
-        USAGE_DEFAULT = 0,
+#pragma warning disable 1591
+        Default = 0,
 
         // buffer allocation policy is platform and usage specific
-        USAGE_ALLOCATE_HOST_MEMORY = 1 << 0,
-        USAGE_ALLOCATE_DEVICE_MEMORY = 1 << 1,
-        USAGE_ALLOCATE_SHARED_MEMORY = 1 << 2, // It is not equal to: USAGE_ALLOCATE_HOST_MEMORY | USAGE_ALLOCATE_DEVICE_MEMORY
-
-        __UMAT_USAGE_FLAGS_32BIT = 0x7fffffff, // Binary compatibility hint
+        HostMemory = 1 << 0,
+        DeviceMemory = 1 << 1,
+        SharedMemory = 1 << 2, // It is not equal to: USAGE_ALLOCATE_HOST_MEMORY | USAGE_ALLOCATE_DEVICE_MEMORY
     }
 }
