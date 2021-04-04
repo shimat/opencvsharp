3addd8488aa494298e181f140b2356376e18eb13
shimat <schimatk@gmail.com>
2021-04-04 22:26:25 +0900

    add ConnectedComponentsAlgorithmsTypes' new values

--- src/OpenCvSharp/Modules/imgproc/Enum/ConnectedComponentsAlgorithmsTypes.cs
+++ src/OpenCvSharp/Modules/imgproc/Enum/ConnectedComponentsAlgorithmsTypes.cs
@@ -21,6 +21,26 @@ namespace OpenCvSharp
         /// <summary>
         /// BBDT algorithm for 8-way connectivity, SAUF algorithm for 4-way connectivity
         /// </summary>
-        GRANA = 1 
+        GRANA = 1,
+        
+        /// <summary>
+        /// Spaghetti @cite Bolelli2019 algorithm for 8-way connectivity, SAUF algorithm for 4-way connectivity.
+        /// </summary>
+        BOLELLI = 2,
+
+        /// <summary>
+        /// Same as CCL_WU. It is preferable to use the flag with the name of the algorithm (CCL_SAUF) rather than the one with the name of the first author (CCL_WU).
+        /// </summary>
+        SAUF = 3,
+
+        /// <summary>
+        /// Same as CCL_GRANA. It is preferable to use the flag with the name of the algorithm (CCL_BBDT) rather than the one with the name of the first author (CCL_GRANA).
+        /// </summary>
+        BBDT = 4,
+
+        /// <summary>
+        /// Same as CCL_BOLELLI. It is preferable to use the flag with the name of the algorithm (CCL_SPAGHETTI) rather than the one with the name of the first author (CCL_BOLELLI).
+        /// </summary>
+        SPAGHETTI = 5,
     }
 }
