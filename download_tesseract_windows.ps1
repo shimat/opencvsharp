8b26452628c406b6ceef175ee5b40a940210ac03
shimat <schimatk@gmail.com>
2021-12-31 18:20:59 +0900

    update tesseract

--- download_tesseract_windows.ps1
+++ download_tesseract_windows.ps1
@@ -1,4 +1,4 @@
-$uriArray =@(
+$uriArray = @(
     "https://github.com/shimat/tesseract_vcpkg/releases/download/2020.12.29/tesseract_vcpkg.0.0.6-beta.zip"
 )
 
@@ -12,9 +12,9 @@ function Download($uri, $outFile) {
 mkdir tesseract_files -Force -ErrorAction Stop | Out-Null
 cd tesseract_files
 
-[Net.ServicePointManager]::SecurityProtocol = @([Net.SecurityProtocolType]::Tls,[Net.SecurityProtocolType]::Tls11,[Net.SecurityProtocolType]::Tls12)
+[Net.ServicePointManager]::SecurityProtocol = @([Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12)
 
-foreach($uri in $uriArray){
+foreach ($uri in $uriArray) {
     $outFile = "tesseract_vcpkg.zip"
     $outFileWithoutExtension = [System.IO.Path]::GetFileNameWithoutExtension($outFile)
     Download $uri $outFile
