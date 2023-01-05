a56a5c2236c27c84a55c1c85db6eafaafba7c628
shimat <schimatk@gmail.com>
2023-01-06 07:16:31 +0900

    try fixing

--- test/OpenCvSharp.Tests/dnn/TensorflowTest.cs
+++ test/OpenCvSharp.Tests/dnn/TensorflowTest.cs
@@ -15,9 +15,9 @@ public class TensorflowTest : TestBase
     // ReSharper disable once IdentifierTypo
     public void LoadMnistTrainingDataFromFile_NetRecognizesAnImageOfA9Correctly()
     {
-        using var img_of_9 = Image(Path.Combine("Dnn","MNIST_9.png"), ImreadModes.Grayscale);
+        using var imgOf9 = Image(Path.Combine("Dnn","MNIST_9.png"), ImreadModes.Grayscale);
             
-        var img9DataBlob = CvDnn.BlobFromImage(img_of_9, 1f / 255.0f);
+        var img9DataBlob = CvDnn.BlobFromImage(imgOf9, 1f / 255.0f);
         var modelPath = Path.Combine("_data", "model", "MNISTTest_tensorflow.pb");
         var res = -1;
 
@@ -37,9 +37,9 @@ public class TensorflowTest : TestBase
     // ReSharper disable once IdentifierTypo
     public void LoadMnistTrainingDataFromStream_NetRecognizesAnImageOfA5Correctly()
     {
-        using var img_of_5 = Image(Path.Combine("Dnn", "MNIST_5.png"), ImreadModes.Grayscale);
+        using var imgOf5 = Image(Path.Combine("Dnn", "MNIST_5.png"), ImreadModes.Grayscale);
 
-        var img5DataBlob = CvDnn.BlobFromImage(img_of_5, 1f / 255.0f);
+        var img5DataBlob = CvDnn.BlobFromImage(imgOf5, 1f / 255.0f);
         var modelPath = Path.Combine("_data", "model", "MNISTTest_tensorflow.pb");
         var res = -1;
 
