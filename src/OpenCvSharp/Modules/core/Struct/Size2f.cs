d12a695c6cf687903071411873439afa227be9cc
n0099 <n@n0099.net>
2023-03-08 18:55:02 +0800

    Update src/OpenCvSharp/Modules/core/Struct/Size2f.cs
    Co-authored-by: shimat <schimatk@gmail.com>

--- src/OpenCvSharp/Modules/core/Struct/Size2f.cs
+++ src/OpenCvSharp/Modules/core/Struct/Size2f.cs
@@ -74,7 +74,7 @@ public struct Size2f : IEquatable<Size2f>
         return new(size.Width, size.Height);
     }
 
-    public static implicit operator Size2f(Size2d size)
+    public static explicit operator Size2f(Size2d size)
     {
         return new(size.Width, size.Height);
     }
