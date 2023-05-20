35b96083e73f57a0d536a01fac495d36b9e7c65a
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2023-05-20 22:59:11 +0300

    Log level names were changed to more readable

--- src/OpenCvSharp/Modules/core/Enum/LogLevel.cs
+++ src/OpenCvSharp/Modules/core/Enum/LogLevel.cs
@@ -8,35 +8,35 @@ public enum LogLevel
     /// <summary>
     /// for using in setLogVevel() call
     /// </summary>
-    LOG_LEVEL_SILENT = 0,
+    SILENT = 0,
 
     /// <summary>
     /// Fatal (critical) error (unrecoverable internal error)
     /// </summary>
-    LOG_LEVEL_FATAL = 1,
+    FATAL = 1,
 
     /// <summary>
     /// Error message.
     /// </summary>
-    LOG_LEVEL_ERROR = 2,
+    ERROR = 2,
 
     /// <summary>
     /// Warning message.
     /// </summary>
-    LOG_LEVEL_WARNING = 3,
+    WARNING = 3,
 
     /// <summary>
     /// Info message.
     /// </summary>
-    LOG_LEVEL_INFO = 4,
+    INFO = 4,
 
     /// <summary>
     /// Debug message. Disabled in the "Release" build.
     /// </summary>
-    LOG_LEVEL_DEBUG = 5,
+    DEBUG = 5,
 
     /// <summary>
     /// Verbose (trace) messages. Requires verbosity level. Disabled in the "Release" build.
     /// </summary>
-    LOG_LEVEL_VERBOSE = 6
+    VERBOSE = 6
 }
