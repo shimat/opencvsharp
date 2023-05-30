d715abf627c11431fc3baa95bfc0bbc5557b5990
zheng zhong <interiv@qq.com>
2023-05-30 13:42:13 +0800

    改了一下

--- src/OpenCvSharp.DebuggerVisualizers/ImageViewer.designer.cs
+++ src/OpenCvSharp.DebuggerVisualizers/ImageViewer.designer.cs
@@ -29,38 +29,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.pictureBox = new System.Windows.Forms.PictureBox();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// pictureBox
-			// 
-			this.pictureBox.BackColor = System.Drawing.Color.Black;
-			this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.pictureBox.Location = new System.Drawing.Point(0, 0);
-			this.pictureBox.Name = "pictureBox";
-			this.pictureBox.Size = new System.Drawing.Size(384, 263);
-			this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-			this.pictureBox.TabIndex = 0;
-			this.pictureBox.TabStop = false;
-			// 
-			// ImageViewer
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.AutoScroll = true;
-			this.ClientSize = new System.Drawing.Size(384, 263);
-			this.Controls.Add(this.pictureBox);
-			this.Name = "ImageViewer";
-			this.Text = "ImageViewer";
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
-			this.ResumeLayout(false);
+            this.SuspendLayout();
+            // 
+            // ImageViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(384, 263);
+            this.Name = "ImageViewer";
+            this.Text = "看图";
+            this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox;
 
 
     }
