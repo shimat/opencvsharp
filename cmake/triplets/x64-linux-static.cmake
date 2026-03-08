42305e60c4192e957489185e1354686adcd76c80
shimat <schimatk@gmail.com>
2026-03-08 19:43:38 +0900

    Fix vcpkg triplet: add VCPKG_CRT_LINKAGE dynamic for Linux

--- cmake/triplets/x64-linux-static.cmake
+++ cmake/triplets/x64-linux-static.cmake
@@ -1,5 +1,6 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 
 # Release-only build: skip debug libraries to reduce build time and artifact size.
